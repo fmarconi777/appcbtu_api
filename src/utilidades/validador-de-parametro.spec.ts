6f48f0b9fe0ac496886db822da3012d78f9ab265
Felipe Pedrosa <marconizero@gmail.com>
2022-03-31 17:38:07 -0300

    teste: garante que o ValidadorDeParametro retorna false se o validador retornar false, retorna true se o validador retornar true e se o validador esta sendo chamado com o parametro correto

---
+++ src/utilidades/validador-de-parametro.spec.ts
@@ -0,0 +1,29 @@
+import { ValidaParametro } from '../apresentacao/protocolos/valida-parametro'
+import { ValidadorDeParametro } from './validador-de-parametro'
+import { validador } from './auxiliares/auxiliar-validador'
+
+const makeSut = (): ValidaParametro => {
+  const sut = new ValidadorDeParametro()
+  return sut
+}
+
+describe('Validador de parametro', () => {
+  test('Deve retornar false se o validador retornar falso', () => {
+    const sut = makeSut()
+    const eValido = sut.validar('sigla_invalida')
+    expect(eValido).toBe(false)
+  })
+
+  test('Deve retornar true se o validador retornar true', () => {
+    const sut = makeSut()
+    const eValido = sut.validar('uel')
+    expect(eValido).toBe(true)
+  })
+
+  test('Deve chamar o validador com o parametro correto', () => {
+    const sut = makeSut()
+    const eSiglaSpy = jest.spyOn(validador, 'eSigla')
+    sut.validar('sigla_qualquer')
+    expect(eSiglaSpy).toHaveBeenCalledWith('sigla_qualquer')
+  })
+})
