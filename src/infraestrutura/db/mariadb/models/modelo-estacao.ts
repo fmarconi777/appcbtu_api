96307133921dd941cfc1e3a62c91e224e39af3bb
Felipe Pedrosa <marconizero@gmail.com>
2022-04-20 14:00:16 -0300

    funcionalidade: adiciona o modelo-estacao para o sequelize referente a tabela Estacao no banco de dados

---
+++ src/infraestrutura/db/mariadb/models/modelo-estacao.ts
@@ -0,0 +1,48 @@
+import { DataTypes, Model, Optional } from 'sequelize'
+import { bd } from '../auxiliares/auxiliar-mariadb'
+import { ModeloEstacao } from '../../../../dominio/modelos/estacao'
+
+interface AtributosEstacao extends Optional<ModeloEstacao, 'id'> {}
+
+export interface InstanciaEstacao extends Model<ModeloEstacao, AtributosEstacao> {}
+
+export const Estacao = bd.define<InstanciaEstacao>(
+  'Estacao',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      unique: true
+    },
+    nome: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    sigla: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    codigo: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    endereco: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    latitude: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    longitude: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    }
+  },
+  {
+    tableName: 'Estacao',
+    timestamps: false
+  }
+)
