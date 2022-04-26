bfffed8b2a94da0f137315cb8ff54f56f3dc7cec
Felipe Pedrosa <marconizero@gmail.com>
2022-04-26 15:24:26 -0300

    teste: garante que RepositorioEstacaoMariaDB retorna todas as estacoes caso um parametro nao for passado

--- src/infraestrutura/db/mariadb/repositorio-estacao/estacao.spec.ts
+++ src/infraestrutura/db/mariadb/repositorio-estacao/estacao.spec.ts
@@ -12,16 +12,50 @@ describe('Repositorio mariaDB Estacao', () => {
     console.log('conexão fechada')
   })
 
-  test('Deve retornar uma estação em caso de sucesso', async () => {
-    const sut = new RepositorioEstacaoMariaDB()
+  const makeSut = (): RepositorioEstacaoMariaDB => {
+    return new RepositorioEstacaoMariaDB()
+  }
+
+  test('Deve retornar uma estação se um parametro for passado', async () => {
+    const sut = makeSut()
+    const resutadoEsperado = {
+      nome: 'Estação São Gabriel',
+      sigla: 'usg',
+      codigo: 'asdf32',
+      endereco: 'rua das flores',
+      latitude: '41.5656',
+      longitude: '85.9696'
+    }
     const estacao = await sut.consulta('usg')
     expect(estacao).toBeTruthy()
     expect(estacao.id).toBeTruthy()
-    expect(estacao.nome).toBe('Estação São Gabriel')
-    expect(estacao.sigla).toBe('usg')
-    expect(estacao.codigo).toBe('asdf32')
-    expect(estacao.endereco).toBe('rua das flores')
-    expect(estacao.latitude).toBe('41.5656')
-    expect(estacao.longitude).toBe('85.9696')
+    expect(estacao).toMatchObject(resutadoEsperado)
+  })
+  test('Deve retornar todas as estações caso um parametro não for passado', async () => {
+    const sut = makeSut()
+    const resutadoEsperado = [
+      {
+        nome: 'Estação São Gabriel',
+        sigla: 'usg',
+        codigo: 'asdf32',
+        endereco: 'rua das flores',
+        latitude: '41.5656',
+        longitude: '85.9696'
+      },
+      {
+        nome: 'Estação Minas Shopping',
+        sigla: 'ums',
+        codigo: 'asdf32',
+        endereco: 'rua dos cravos',
+        latitude: '39.4141',
+        longitude: '86.3478'
+      }
+    ]
+    const estacao = await sut.consulta()
+    expect(estacao).toBeTruthy()
+    expect(estacao[0].id).toBeTruthy()
+    expect(estacao[1].id).toBeTruthy()
+    expect(estacao[0]).toMatchObject(resutadoEsperado[0])
+    expect(estacao[1]).toMatchObject(resutadoEsperado[1])
   })
 })
