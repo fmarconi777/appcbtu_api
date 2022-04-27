c7ff1b0e87a03aec513b47543b8e993357849c3b
Felipe Pedrosa <marconizero@gmail.com>
2022-04-27 10:45:47 -0300

    funcionalidade: garante que o cors esta habilitado

--- src/principal/config/middlewares.ts
+++ src/principal/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { transformaCorpo } from '../middlewares/transforma-corpo'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(transformaCorpo)
+  app.use(cors)
 }
---
+++ src/principal/middlewares/cors.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CORS middleware', () => {
+  test('Deve habilitar o CORS', async () => {
+    app.get('/teste_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/teste_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
