17aec533df5e2894f4f424271b7b48b5505f4b92
Felipe Pedrosa <marconizero@gmail.com>
2022-04-27 11:10:00 -0300

    refatoracao: muda o nome do teste do middleware transformaCorpo

--- src/principal/middlewares/transforma-corpo.test.ts
+++ src/principal/middlewares/transforma-corpo.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Transforma corpo middleware', () => {
-  test('Deve transformar o corpo da resposta para o formato json', async () => {
+  test('Deve transformar o corpo da requisicao do formato json para objeto', async () => {
     app.post('/teste_transforma_corpo', (req, res) => {
       res.send(req.body)
     })
