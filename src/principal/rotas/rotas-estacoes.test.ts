279c676e14b58666d38ad67f9a32b5d01c92c0d2
Felipe Pedrosa <marconizero@gmail.com>
2022-04-28 11:37:08 -0300

    refatoracao: conecta ao mariadb antes do teste de integracao

--- src/principal/rotas/rotas-estacoes.test.ts
+++ src/principal/rotas/rotas-estacoes.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest'
 import app from '../config/app'
+import { bd } from '../../infraestrutura/db/mariadb/auxiliares/auxiliar-mariadb'
 
 describe('Rotas estações', () => {
+  beforeAll(async () => {
+    await bd.authenticate()
+    console.log('conexão aberta')
+  })
+
+  afterAll(async () => {
+    await bd.close()
+    console.log('conexão fechada')
+  })
+
   test('Deve retornar todas as estações em caso de sucesso', async () => {
     await request(app)
       .get('/estacao')
