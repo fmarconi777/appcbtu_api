fe4c81abdf2a26f0fb67fb0c227d9ec30e7284e9
Felipe Pedrosa <marconizero@gmail.com>
2022-04-28 11:38:48 -0300

    funcionalidade: conecta ao mariadb de iniciar o app

--- src/principal/servidor.ts
+++ src/principal/servidor.ts
@@ -1,3 +1,11 @@
-import app from './config/app'
+import { bd } from '../infraestrutura/db/mariadb/auxiliares/auxiliar-mariadb'
 
-app.listen(5000, () => console.log('Servidor rodando no endereço http://localhost:5000'))
+bd.authenticate()
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    console.log('Conexão estabelecida com sucesso.')
+    app.listen(5000, () => console.log('Servidor rodando no endereço http://localhost:5000'))
+  })
+  .catch((error) => {
+    console.error('Não foi possível conectar com o banco de dados:', error)
+  })
