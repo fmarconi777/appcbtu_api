3b2aea45938d800c617f17538e9def24fbb4f33a
Vicilius <viniciusmachado19@gmail.com>
2022-05-04 11:56:33 -0300

    teste: garante que o controlador cadastro retorne codigo 400 quando estiver faltando parametro

--- src/apresentacao/controladores/cadastro.spec.ts
+++ src/apresentacao/controladores/cadastro.spec.ts
@@ -1,8 +1,12 @@
 import { ControladorDeCadastro } from './cadastro'
 
+const makeSut = (): ControladorDeCadastro => {
+  return new ControladorDeCadastro()
+}
+
 describe('Controlador de Cadastro', () => {
   test('Retornar 400 quando o nome não for fornecido', () => {
-    const sut = new ControladorDeCadastro()
+    const sut = makeSut()
     const requisicaoHttp = {
       corpo: {
         area: 'qualquer_area',
@@ -14,5 +18,61 @@ describe('Controlador de Cadastro', () => {
     const respostaHttp = sut.tratar(requisicaoHttp)
     expect(respostaHttp.status).toBe(400)
     expect(respostaHttp.corpo).toEqual(new Error('Falta parametro: nome '))
+  })
+  test('Retornar 400 quando o email não for fornecido', () => {
+    const sut = makeSut()
+    const requisicaoHttp = {
+      corpo: {
+        nome: 'qualquer_nome',
+        area: 'qualquer_area',
+        senha: 'qualquer_senha',
+        confirmarSenha: 'qualquer_senha'
+      }
+    }
+    const respostaHttp = sut.tratar(requisicaoHttp)
+    expect(respostaHttp.status).toBe(400)
+    expect(respostaHttp.corpo).toEqual(new Error('Falta parametro: email '))
+  })
+  test('Retornar 400 quando a area não for fornecido', () => {
+    const sut = makeSut()
+    const requisicaoHttp = {
+      corpo: {
+        nome: 'qualquer_nome',
+        email: 'qualquer_email@mail.com',
+        senha: 'qualquer_senha',
+        confirmarSenha: 'qualquer_senha'
+      }
+    }
+    const respostaHttp = sut.tratar(requisicaoHttp)
+    expect(respostaHttp.status).toBe(400)
+    expect(respostaHttp.corpo).toEqual(new Error('Falta parametro: area '))
+  })
+  test('Retornar 400 quando a senha não for fornecido', () => {
+    const sut = makeSut()
+    const requisicaoHttp = {
+      corpo: {
+        nome: 'qualquer_nome',
+        area: 'qualquer_area',
+        email: 'qualquer_email@mail.com',
+        confirmarSenha: 'qualquer_senha'
+      }
+    }
+    const respostaHttp = sut.tratar(requisicaoHttp)
+    expect(respostaHttp.status).toBe(400)
+    expect(respostaHttp.corpo).toEqual(new Error('Falta parametro: senha '))
+  })
+  test('Retornar 400 quando o confirmarsenha não for fornecido', () => {
+    const sut = makeSut()
+    const requisicaoHttp = {
+      corpo: {
+        nome: 'qualquer_nome',
+        area: 'qualquer_area',
+        email: 'qualquer_email@mail.com',
+        senha: 'qualquer_senha'
+      }
+    }
+    const respostaHttp = sut.tratar(requisicaoHttp)
+    expect(respostaHttp.status).toBe(400)
+    expect(respostaHttp.corpo).toEqual(new Error('Falta parametro: confirmarSenha '))
   })
 })
