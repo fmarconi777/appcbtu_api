6acf543ce589a7adb74a8bb059c11625ccddf3b8
Vicilius <viniciusmachado19@gmail.com>
2022-05-04 11:56:01 -0300

    funcionalidade: garante que o controlador cadastro retorne codigo 400 quando estiver faltando parametro

--- src/apresentacao/controladores/cadastro.ts
+++ src/apresentacao/controladores/cadastro.ts
@@ -1,10 +1,36 @@
-import { RequisicaoHttp, RespostaHttp } from '../protocolos/http'
+import { RequisicaoHttp } from '../protocolos/http'
 
 export class ControladorDeCadastro {
-  tratar (requisicaoHttp: RequisicaoHttp): RespostaHttp {
-    return {
-      status: 400,
-      corpo: new Error('Falta parametro: nome ')
+  tratar (requisicaoHttp: RequisicaoHttp): any {
+    if (!requisicaoHttp.corpo.nome) { // eslint-disable-line
+      return {
+        status: 400,
+        corpo: new Error('Falta parametro: nome ')
+      }
+    }
+    if (!requisicaoHttp.corpo.email) { // eslint-disable-line
+      return {
+        status: 400,
+        corpo: new Error('Falta parametro: email ')
+      }
+    }
+    if (!requisicaoHttp.corpo.area) { // eslint-disable-line
+      return {
+        status: 400,
+        corpo: new Error('Falta parametro: area ')
+      }
+    }
+    if (!requisicaoHttp.corpo.senha) { // eslint-disable-line
+      return {
+        status: 400,
+        corpo: new Error('Falta parametro: senha ')
+      }
+    }
+    if (!requisicaoHttp.corpo.confirmarSenha) { // eslint-disable-line
+      return {
+        status: 400,
+        corpo: new Error('Falta parametro: confirmarSenha ')
+      }
     }
   }
 }
