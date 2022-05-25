3f5655ffdb6d46b8eb9302b99b49e9a241f0334a
Vicilius <viniciusmachado19@gmail.com>
2022-05-25 09:47:32 -0300

    Funcionalidade: Fabrica de funcionarios para que funcione as rotas do funcionarios-rotas quando o adaptador pegar a função

---
+++ src/principal/fabrica/funcionario.ts
@@ -0,0 +1,18 @@
+import { ControladorDeFuncionario } from '../../apresentacao/controladores/funcionario'
+import { ValidadorDeEmailAdaptador } from '../../utilidades/auxiliares/validador-de-email'
+import { BdAdicionarConta } from '../../dados/casos-de-uso/bdadicionarconta/bd-adicionar-conta'
+import { BcryptAdaptador } from '../../infraestrutura/criptografia/bcrypt-adaptador'
+import { DecoradorControladorLog } from '../decoradores/log'
+import { Controlador } from '../../apresentacao/protocolos/controlador'
+import { RepositorioLogDeErroMariaDB } from '../../infraestrutura/bd/mariadb/repositorio/log'
+import { RepositorioFuncionarioMariaDB } from '../../infraestrutura/bd/mariadb/repositorio/funcionario'
+export const criaControladorDeFuncionario = (): Controlador => {
+  const salt = 12
+  const validadorDeEmail = new ValidadorDeEmailAdaptador()
+  const bcryptAdaptador = new BcryptAdaptador(salt)
+  const repositorioFuncionario = new RepositorioFuncionarioMariaDB()
+  const bdAdiconarConta = new BdAdicionarConta(bcryptAdaptador, repositorioFuncionario)
+  const controladorFuncionario = new ControladorDeFuncionario(validadorDeEmail, bdAdiconarConta)
+  const repositorioLogDeErro = new RepositorioLogDeErroMariaDB()
+  return new DecoradorControladorLog(controladorFuncionario, repositorioLogDeErro)
+}
