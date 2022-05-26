a661b84d2243233ec7d3af070b637ec49788d9a9
Felipe Pedrosa <marconizero@gmail.com>
2022-05-26 17:52:16 -0300

    refatoracao: corigida a rota para cadstro de funcionario

--- src/principal/rotas/funcionarios-rotas.ts
+++ src/principal/rotas/funcionarios-rotas.ts
@@ -3,6 +3,6 @@ import { adaptadorDeRota } from '../adaptadores/adaptador-de-rota-express'
 import { criaControladorDeFuncionario } from '../fabrica/funcionario'
 
 export default (router: Router): Router => {
-  router.get('/funcionarios/:parametro?', adaptadorDeRota(criaControladorDeFuncionario())) // eslint-disable-line
+  router.post('/funcionario', adaptadorDeRota(criaControladorDeFuncionario())) // eslint-disable-line
   return router
 }
