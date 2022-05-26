604aef5c13187c43dd08e4849d3b3c7692539513
Felipe Pedrosa <marconizero@gmail.com>
2022-05-26 15:26:38 -0300

    teste: garante que o AutenticadorBD retorna null caso o ComparadorHash retornar false

--- src/dados/casos-de-uso/autenticador/autenticador-bd.spec.ts
+++ src/dados/casos-de-uso/autenticador/autenticador-bd.spec.ts
@@ -32,25 +32,25 @@ const makeComparadorHash = (): ComparadorHash => {
 
 interface SubTipos {
   sut: Autenticador
-  repositorioConsultaFuncionarioPorEmail: RepositorioConsultaFuncionarioPorEmail
+  repositorioConsultaFuncionarioPorEmailStub: RepositorioConsultaFuncionarioPorEmail
   comparadorHashStub: ComparadorHash
 }
 
 const makeSut = (): SubTipos => {
-  const repositorioConsultaFuncionarioPorEmail = makeRepositorioConsultaFuncionarioPorEmail()
+  const repositorioConsultaFuncionarioPorEmailStub = makeRepositorioConsultaFuncionarioPorEmail()
   const comparadorHashStub = makeComparadorHash()
-  const sut = new AutenticadorBD(repositorioConsultaFuncionarioPorEmail, comparadorHashStub)
+  const sut = new AutenticadorBD(repositorioConsultaFuncionarioPorEmailStub, comparadorHashStub)
   return {
     sut,
-    repositorioConsultaFuncionarioPorEmail,
+    repositorioConsultaFuncionarioPorEmailStub,
     comparadorHashStub
   }
 }
 
 describe('Autenticação no banco de dados', () => {
   test('Deve chamar RepositorioConsultaFuncionarioPorEmail com o email correto', async () => {
-    const { sut, repositorioConsultaFuncionarioPorEmail } = makeSut()
-    const consultaSpy = jest.spyOn(repositorioConsultaFuncionarioPorEmail, 'consulta')
+    const { sut, repositorioConsultaFuncionarioPorEmailStub } = makeSut()
+    const consultaSpy = jest.spyOn(repositorioConsultaFuncionarioPorEmailStub, 'consulta')
     const autenticacao = {
       email: 'email_qualquer@mail.com',
       senha: 'senha_qualquer'
@@ -60,8 +60,8 @@ describe('Autenticação no banco de dados', () => {
   })
 
   test('Deve retornar um erro caso o RepositorioConsultaFuncionarioPorEmail retorne um erro', async () => {
-    const { sut, repositorioConsultaFuncionarioPorEmail } = makeSut()
-    jest.spyOn(repositorioConsultaFuncionarioPorEmail, 'consulta').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const { sut, repositorioConsultaFuncionarioPorEmailStub } = makeSut()
+    jest.spyOn(repositorioConsultaFuncionarioPorEmailStub, 'consulta').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const autenticacao = {
       email: 'email_qualquer@mail.com',
       senha: 'senha_qualquer'
@@ -71,8 +71,8 @@ describe('Autenticação no banco de dados', () => {
   })
 
   test('Deve retornar null caso o RepositorioConsultaFuncionarioPorEmail retorne null', async () => {
-    const { sut, repositorioConsultaFuncionarioPorEmail } = makeSut()
-    jest.spyOn(repositorioConsultaFuncionarioPorEmail, 'consulta').mockReturnValueOnce(null)
+    const { sut, repositorioConsultaFuncionarioPorEmailStub } = makeSut()
+    jest.spyOn(repositorioConsultaFuncionarioPorEmailStub, 'consulta').mockReturnValueOnce(null)
     const autenticacao = {
       email: 'email_qualquer@mail.com',
       senha: 'senha_qualquer'
@@ -101,5 +101,16 @@ describe('Autenticação no banco de dados', () => {
     }
     const promise = sut.autenticar(autenticacao)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Deve retornar null caso o ComparadorHash retorne false', async () => {
+    const { sut, comparadorHashStub } = makeSut()
+    jest.spyOn(comparadorHashStub, 'comparar').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const autenticacao = {
+      email: 'email_qualquer@mail.com',
+      senha: 'senha_qualquer'
+    }
+    const tokenDeAcesso = await sut.autenticar(autenticacao)
+    expect(tokenDeAcesso).toBeNull()
   })
 })
