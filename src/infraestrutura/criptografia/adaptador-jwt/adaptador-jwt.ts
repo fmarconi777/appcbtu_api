e3a857b894a6095c37393654fccb9fa330d0ef50
Felipe Pedrosa <marconizero@gmail.com>
2022-05-27 16:42:56 -0300

    funcionalidade: garante que o AdaptadorJwt retorna um token em caso de sucesso

--- src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.ts
+++ src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.ts
@@ -6,7 +6,7 @@ export class AdapatadorJwt implements Encriptador {
   private readonly chaveSecreta = process.env.CHAVE_SECRETA
 
   async encriptar (valor: string): Promise<string> {
-    jwt.sign({ id: valor }, (this.chaveSecreta as string))
-    return await new Promise(resolve => resolve(''))
+    const tokenDeAcesso = jwt.sign({ id: valor }, (this.chaveSecreta as string))
+    return tokenDeAcesso
   }
 }
