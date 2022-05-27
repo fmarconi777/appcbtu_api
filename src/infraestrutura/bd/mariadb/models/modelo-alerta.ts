0af444210b6aeb69d43796d43cc53d99aa1c70b2
Vicilius <viniciusmachado19@gmail.com>
2022-05-27 10:39:19 -0300

    Modelo: modelo alerta criado

---
+++ src/infraestrutura/bd/mariadb/models/modelo-alerta.ts
@@ -0,0 +1,56 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from 'sequelize'
+import { bd as sequelize } from '../auxiliares/auxiliar-mariadb'
+
+export class Alerta extends Model<
+InferAttributes<Alerta>,
+InferCreationAttributes<Alerta>
+> {
+  declare id: CreationOptional<number>
+  declare descricao: string
+  declare prioridade: string
+  declare dataInicio: string
+  declare dataFim: string
+  declare ativo: string
+  declare estacaoId: number
+}
+
+Alerta.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      unique: true
+    },
+    descricao: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    prioridade: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    dataInicio: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    dataFim: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    ativo: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    estacaoId: {
+      allowNull: false,
+      type: DataTypes.UUID
+    }
+  },
+  {
+    sequelize,
+    tableName: 'Alerta',
+    timestamps: false
+  }
+)
