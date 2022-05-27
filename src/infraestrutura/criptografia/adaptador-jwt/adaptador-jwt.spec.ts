82e99b6af9dcc03cbf765cf4883aca174ec26fbf
Felipe Pedrosa <marconizero@gmail.com>
2022-05-27 16:52:09 -0300

    refatoracao: corrigido implementacao do makeSut

--- src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.spec.ts
+++ src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.spec.ts
@@ -11,33 +11,27 @@ jest.mock('jsonwebtoken', () => ({
   }
 }))
 
-interface SubTipos {
-  sut: Encriptador
-}
-
-const makeSut = (): SubTipos => {
+const makeSut = (): Encriptador => {
   const sut = new AdapatadorJwt()
-  return {
-    sut
-  }
+  return sut
 }
 
 describe('Adaptador do jwt', () => {
   test('Deve chamar o sign do jwt com o valores corretos', async () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encriptar('id_qualquer')
     expect(signSpy).toHaveBeenCalledWith({ id: 'id_qualquer' }, chaveSecreta)
   })
 
   test('Deve retornar um token em caso de sucesso', async () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const tokenDeAcesso = await sut.encriptar('id_qualquer')
     expect(tokenDeAcesso).toBe('token_qualquer')
   })
 
   test('Deve retornar um erro caso o sign retorne um erro', async () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => Promise.reject(new Error())) // eslint-disable-line
     const tokenDeAcesso = sut.encriptar('id_qualquer')
     await expect(tokenDeAcesso).rejects.toThrow()
