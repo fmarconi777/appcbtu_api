965a447074ae6e37c1458ad16ca1ba9c02358309
Felipe Pedrosa <marconizero@gmail.com>
2022-05-27 14:02:23 -0300

    teste: garante que o BcryptAdaptador chama o metodo comparar com os valores corretos

--- src/infraestrutura/criptografia/bcrypt-adaptador.spec.ts
+++ src/infraestrutura/criptografia/bcrypt-adaptador.spec.ts
@@ -5,6 +5,10 @@ import 'dotenv/config'
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return await new Promise(resolve => resolve('hash'))
+  },
+
+  async compare (): Promise<boolean> {
+    return await new Promise(resolve => resolve(true))
   }
 }))
 
@@ -12,22 +16,32 @@ const salt = process.env.SALT
 const makeSut = (): BcryptAdaptador => {
   return new BcryptAdaptador()
 }
+
 describe('Bcrypt Adaptador', () => {
-  test('Deverá chamar o bcrypt com valores corretos', async () => {
+  test('Deverá chamar o método gerar com os valores corretos', async () => {
     const sut = makeSut()
     const hashEspionar = jest.spyOn(bcrypt, 'hash')
     await sut.gerar('qualquer_valor')
     expect(hashEspionar).toHaveBeenCalledWith('qualquer_valor', +(salt as string))
   })
-  test('Deverá retornar um hash em caso de sucesso', async () => {
+
+  test('Deverá retornar um hash válido em caso de sucesso', async () => {
     const sut = makeSut()
     const hash = await sut.gerar('qualquer_valor')
     expect(hash).toBe('hash')
   })
-  test('Deverá retornar condição throw caso bcrypt esteja em throw ', async () => {
+
+  test('Deverá retornar um erro caso bcrypt retorne um erro', async () => {
     const sut = makeSut()
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => Promise.reject(new Error())) // eslint-disable-line
     const promise = sut.gerar('qualquer_valor')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Deverá chamar o método comparar com valores corretos', async () => {
+    const sut = makeSut()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.comparar('qualquer_valor', 'hash_qualquer')
+    expect(compareSpy).toHaveBeenCalledWith('qualquer_valor', 'hash_qualquer')
   })
 })
