e9767be89f36a415a1317808755f153151f2a3c1
Felipe Pedrosa <marconizero@gmail.com>
2022-06-01 09:18:32 -0300

    refatoracao: adiciona expiracao ao token

--- src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.ts
+++ src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.ts
@@ -6,7 +6,7 @@ export class AdapatadorJwt implements Encriptador {
   private readonly chaveSecreta = process.env.CHAVE_SECRETA
 
   async encriptar (valor: string): Promise<string> {
-    const tokenDeAcesso = jwt.sign({ id: valor }, (this.chaveSecreta as string))
+    const tokenDeAcesso = jwt.sign({ id: valor }, (this.chaveSecreta as string), { expiresIn: '4h' })
     return tokenDeAcesso
   }
 }
