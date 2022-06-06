51186f50c3c3023a12d44dccab672424af42dece
Felipe Pedrosa <marconizero@gmail.com>
2022-06-06 15:58:53 -0300

    refatoracao: criaControladorDeFuncionario recebe mais um parametro do tipo RepositorioConsultaFuncionarioPorEmail

--- src/principal/fabrica/funcionario.ts
+++ src/principal/fabrica/funcionario.ts
@@ -1,16 +1,17 @@
 import { ControladorDeFuncionario } from '../../apresentacao/controladores/funcionario'
-import { ValidadorDeEmailAdaptador } from '../../utilidades/validador-de-email'
+import { ValidadorDeEmailAdaptador } from '../../utilidades/validadores/validador-de-email'
 import { BdAdicionarConta } from '../../dados/casos-de-uso/funcionario/bd-adicionar-conta'
 import { BcryptAdaptador } from '../../infraestrutura/criptografia/bcrypt-adaptador/bcrypt-adaptador'
 import { DecoradorControladorLog } from '../decoradores/log'
 import { Controlador } from '../../apresentacao/protocolos/controlador'
 import { RepositorioLogDeErroMariaDB } from '../../infraestrutura/bd/mariadb/repositorio/log'
 import { RepositorioFuncionarioMariaDB } from '../../infraestrutura/bd/mariadb/repositorio/funcionario'
+
 export const criaControladorDeFuncionario = (): Controlador => {
   const validadorDeEmail = new ValidadorDeEmailAdaptador()
   const bcryptAdaptador = new BcryptAdaptador()
   const repositorioFuncionario = new RepositorioFuncionarioMariaDB()
-  const bdAdiconarConta = new BdAdicionarConta(bcryptAdaptador, repositorioFuncionario)
+  const bdAdiconarConta = new BdAdicionarConta(bcryptAdaptador, repositorioFuncionario, repositorioFuncionario)
   const controladorFuncionario = new ControladorDeFuncionario(validadorDeEmail, bdAdiconarConta)
   const repositorioLogDeErro = new RepositorioLogDeErroMariaDB()
   return new DecoradorControladorLog(controladorFuncionario, repositorioLogDeErro)
