cb90c1088650a70bd474a71769524665d47d688e
Felipe Pedrosa <marconizero@gmail.com>
2022-06-06 14:56:59 -0300

    funcionalidade: garante que o BdAdicionarConta chama o RepositorioConsultaFuncionarioPorEmail com o email correto

--- src/dados/casos-de-uso/funcionario/bd-adicionar-conta.ts
+++ src/dados/casos-de-uso/funcionario/bd-adicionar-conta.ts
@@ -2,16 +2,21 @@ import { CadastroDeFuncionario, InserirModeloFuncionario } from '../../../domini
 import { ModeloFuncionario } from '../../../dominio/modelos/funcionario'
 import { GeradorDeHash } from '../../protocolos/criptografia/gerador-de-hash'
 import { RepositorioFuncionario } from '../../protocolos/bd/repositorio-funcionario'
+import { RepositorioConsultaFuncionarioPorEmail } from '../../protocolos/bd/repositorio-consulta-funcionario-por-email'
 
 export class BdAdicionarConta implements CadastroDeFuncionario {
   private readonly geradorDeHash: GeradorDeHash
   private readonly repositorioFuncionario: RepositorioFuncionario
-  constructor (geradorDeHash: GeradorDeHash, RepositorioFuncionario: RepositorioFuncionario) {
+  private readonly repositorioConsultaFuncionarioPorEmail: RepositorioConsultaFuncionarioPorEmail
+
+  constructor (geradorDeHash: GeradorDeHash, RepositorioFuncionario: RepositorioFuncionario, repositorioConsultaFuncionarioPorEmail: RepositorioConsultaFuncionarioPorEmail) {
     this.geradorDeHash = geradorDeHash
     this.repositorioFuncionario = RepositorioFuncionario
+    this.repositorioConsultaFuncionarioPorEmail = repositorioConsultaFuncionarioPorEmail
   }
 
-  async adicionar (contaData: InserirModeloFuncionario): Promise<ModeloFuncionario> {
+  async adicionar (contaData: InserirModeloFuncionario): Promise<ModeloFuncionario | null> {
+    await this.repositorioConsultaFuncionarioPorEmail.consultaPorEmail(contaData.email)
     const senhaHashed = await this.geradorDeHash.gerar(contaData.senha)
     const conta = await this.repositorioFuncionario.adicionar(Object.assign({}, contaData, { senha: senhaHashed }))
     return Object.assign({}, conta, { senha: undefined })
