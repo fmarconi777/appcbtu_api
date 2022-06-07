bc7b5ee3861e7fb0f4042af075f0048726230c47
Felipe Pedrosa <marconizero@gmail.com>
2022-06-07 10:45:28 -0300

    refatoracao: renomeado metodo consulta para consultar

--- src/dados/casos-de-uso/estacao/consulta-estacao-bd.spec.ts
+++ src/dados/casos-de-uso/estacao/consulta-estacao-bd.spec.ts
@@ -3,7 +3,7 @@ import { ConsultaEstacaoBD } from './consulta-estacao-bd'
 
 const makeRepositorioEstacao = (): RepositorioEstacao => {
   class RepositorioEstacaoStub implements RepositorioEstacao {
-    async consulta (sigla?: string): Promise<ModelosEstacoes> {
+    async consultar (sigla?: string): Promise<ModelosEstacoes> {
       if (!sigla) { // eslint-disable-line
         return await new Promise(resolve => resolve([{
           id: 'id_valida',
@@ -46,30 +46,30 @@ const makeSut = (): SutTypes => {
 describe('Caso de uso ConsultaEstacaoBD', () => {
   test('Deve chamar RepositorioEstacao com o valor correto', async () => {
     const { sut, RepositorioEstacaoStub } = makeSut()
-    const consultaSpy = jest.spyOn(RepositorioEstacaoStub, 'consulta')
+    const consultaSpy = jest.spyOn(RepositorioEstacaoStub, 'consultar')
     const sigla = 'sigla_qualquer'
-    await sut.consulta(sigla)
+    await sut.consultar(sigla)
     expect(consultaSpy).toHaveBeenCalledWith('sigla_qualquer')
   })
 
   test('Metodo consulta deve retornar um erro se o RepositorioEstacao retornar um erro', async () => {
     const { sut, RepositorioEstacaoStub } = makeSut()
-    jest.spyOn(RepositorioEstacaoStub, 'consulta').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(RepositorioEstacaoStub, 'consultar').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const sigla = 'sigla_qualquer'
-    const consulta = sut.consulta(sigla)
+    const consulta = sut.consultar(sigla)
     await expect(consulta).rejects.toThrow()
   })
 
   test('Metodo consultaTodas deve retornar um erro se o RepositorioEstacao retornar um erro', async () => {
     const { sut, RepositorioEstacaoStub } = makeSut()
-    jest.spyOn(RepositorioEstacaoStub, 'consulta').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const consulta = sut.consultaTodas()
+    jest.spyOn(RepositorioEstacaoStub, 'consultar').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const consulta = sut.consultarTodas()
     await expect(consulta).rejects.toThrow()
   })
 
   test('Deve retornar array com todas as estações se um parâmetro não for fornecido', async () => {
     const { sut } = makeSut()
-    const consulta = await sut.consultaTodas()
+    const consulta = await sut.consultarTodas()
     expect(consulta).toEqual([{
       id: 'id_valida',
       nome: 'nome_valido',
@@ -84,7 +84,7 @@ describe('Caso de uso ConsultaEstacaoBD', () => {
   test('Deve retornar uma estação se um parâmetro for fornecido', async () => {
     const { sut } = makeSut()
     const sigla = 'sigla_valida'
-    const consulta = await sut.consulta(sigla)
+    const consulta = await sut.consultar(sigla)
     expect(consulta).toEqual({
       id: 'id_valida',
       nome: 'nome_valido',
