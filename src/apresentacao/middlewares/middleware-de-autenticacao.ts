e6e8a43ab34a360837e138ec083c46600d8a93ed
Felipe Pedrosa <marconizero@gmail.com>
2022-06-07 14:31:39 -0300

    funcionalidade: garante que o MiddlewareDeAutenticacao retorna status 500 se ConsultaFuncionarioPeloToken retornar um erro

--- src/apresentacao/middlewares/middleware-de-autenticacao.ts
+++ src/apresentacao/middlewares/middleware-de-autenticacao.ts
@@ -1,6 +1,6 @@
 import { Middleware } from '../protocolos/middleware'
 import { RequisicaoHttp, RespostaHttp } from '../protocolos/http'
-import { requisicaoNegada, resposta } from '../auxiliares/auxiliar-http'
+import { erroDeServidor, requisicaoNegada, resposta } from '../auxiliares/auxiliar-http'
 import { ErroAcessoNegado } from '../erros/erro-acesso-negado'
 import { ConsultaFuncionarioPeloToken } from '../../dominio/casos-de-uso/middleware/consulta-funcionario-por-token'
 
@@ -10,13 +10,17 @@ export class MiddlewareDeAutenticacao implements Middleware {
   ) {}
 
   async tratar (requisicaoHttp: RequisicaoHttp): Promise<RespostaHttp> {
-    const tokenDeAcesso = requisicaoHttp.cabecalho?.['x-access-token']
-    if (tokenDeAcesso) { //eslint-disable-line
-      const funcionario = await this.consultaFuncionarioPeloToken.consultar(tokenDeAcesso)
-      if (funcionario) { //eslint-disable-line
-        return resposta({ IdFuncionario: funcionario.id })
+    try {
+      const tokenDeAcesso = requisicaoHttp.cabecalho?.['x-access-token']
+      if (tokenDeAcesso) { //eslint-disable-line
+        const funcionario = await this.consultaFuncionarioPeloToken.consultar(tokenDeAcesso)
+        if (funcionario) { //eslint-disable-line
+          return resposta({ IdFuncionario: funcionario.id })
+        }
       }
+      return requisicaoNegada(new ErroAcessoNegado())
+    } catch (erro: any) {
+      return erroDeServidor(erro)
     }
-    return requisicaoNegada(new ErroAcessoNegado())
   }
 }
