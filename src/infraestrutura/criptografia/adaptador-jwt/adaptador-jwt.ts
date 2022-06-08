02cda389e19a9da9960b8fda204b81dc0a2c4930
Felipe Pedrosa <marconizero@gmail.com>
2022-06-08 15:00:01 -0300

    funcionaldade: garante que o AdapatadorJwt chama o metodo verify com os valores corretos

--- src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.ts
+++ src/infraestrutura/criptografia/adaptador-jwt/adaptador-jwt.ts
@@ -1,12 +1,18 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
+import { Decriptador } from '../../../dados/protocolos/criptografia/decriptador'
 import { Encriptador } from '../../../dados/protocolos/criptografia/encriptador'
 
-export class AdapatadorJwt implements Encriptador {
+export class AdapatadorJwt implements Encriptador, Decriptador {
   private readonly chaveSecreta = process.env.CHAVE_SECRETA
 
   async encriptar (valor: string): Promise<string> {
     const tokenDeAcesso = jwt.sign({ id: valor }, (this.chaveSecreta as string), { expiresIn: '4h' })
     return tokenDeAcesso
+  }
+
+  async decriptar (valor: string): Promise<string | null> {
+    jwt.verify(valor, (this.chaveSecreta as string))
+    return null
   }
 }
