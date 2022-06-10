bea59f0630167757a1521b7ad48c66318a929892
Felipe Pedrosa <marconizero@gmail.com>
2022-06-10 14:54:03 -0300

    teste: garante que a rota de cadastro de equipamentos retorna status 403 caso o authorization venha sem um token

--- src/principal/rotas/rotas-equipamentos.test.ts
+++ src/principal/rotas/rotas-equipamentos.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest'
 import app from '../config/app'
 import { AuxiliaresMariaDB } from '../../infraestrutura/bd/mariadb/auxiliares/auxiliar-mariadb'
 import { Equipamento } from '../../infraestrutura/bd/mariadb/models/modelo-equipamento'
+import { Funcionario } from '../../infraestrutura/bd/mariadb/models/modelo-funcionarios'
+import 'dotenv/config'
 
 describe('Rotas equipamentos', () => {
   beforeAll(async () => {
@@ -16,11 +18,26 @@ describe('Rotas equipamentos', () => {
 
   beforeEach(async () => {
     await Equipamento.destroy({ truncate: true, cascade: false })
+    await Funcionario.destroy({ truncate: true, cascade: false })
   })
 
-  test('Deve retornar um status 403 ao adicionar um equipamento sem token de acesso', async () => {
+  test('Deve retornar um status 403 ao adicionar um equipamento sem autenticação', async () => {
     await request(app)
       .post('/equipamento')
+      .send({
+        nome: 'Escada rolante',
+        tipo: 'escada',
+        numFalha: '35647',
+        estado: '1',
+        estacaoId: '1'
+      })
+      .expect(403)
+  })
+
+  test('Deve retornar um status 403 ao adicionar um equipamento com authorization sem token de acesso', async () => {
+    await request(app)
+      .post('/equipamento')
+      .set('authorization', 'Bearer ')
       .send({
         nome: 'Escada rolante',
         tipo: 'escada',
