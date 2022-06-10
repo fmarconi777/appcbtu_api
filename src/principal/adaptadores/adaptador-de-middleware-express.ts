2c0fc7ace56254a473ce4115d5be0f06f9003101
Felipe Pedrosa <marconizero@gmail.com>
2022-06-10 14:53:50 -0300

    funcionalidade: garante que a rota de cadastro de equipamentos retorna status 403 caso o authorization venha sem um token

--- src/principal/adaptadores/adaptador-de-middleware-express.ts
+++ src/principal/adaptadores/adaptador-de-middleware-express.ts
@@ -1,20 +1,27 @@
 import { NextFunction, Request, Response } from 'express'
+import { requisicaoNegada } from '../../apresentacao/auxiliares/auxiliar-http'
+import { ErroAcessoNegado } from '../../apresentacao/erros/erro-acesso-negado'
 import { RequisicaoHttp } from '../../apresentacao/protocolos/http'
 import { Middleware } from '../../apresentacao/protocolos/middleware'
 
 export const adaptadorDeMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const autorizacao: any = req.headers.authorization
-    const [, tokenDeAcesso] = autorizacao?.split(' ')
-    const requisicaoHttp: RequisicaoHttp = {
-      cabecalho: tokenDeAcesso
-    }
-    const respostaHttp = await middleware.tratar(requisicaoHttp)
-    if (respostaHttp.status === 200) {
-      Object.assign(req, respostaHttp.corpo)
-      next()
+    if (autorizacao) { // eslint-disable-line
+      const [, tokenDeAcesso] = autorizacao?.split(' ')
+      const requisicaoHttp: RequisicaoHttp = {
+        cabecalho: tokenDeAcesso
+      }
+      const respostaHttp = await middleware.tratar(requisicaoHttp)
+      if (respostaHttp.status === 200) {
+        Object.assign(req, respostaHttp.corpo)
+        next()
+      } else {
+        res.status(respostaHttp.status).json({ erro: respostaHttp.corpo.message })
+      }
     } else {
-      res.status(respostaHttp.status).json({ erro: respostaHttp.corpo.message })
+      const respostaHttp = requisicaoNegada(new ErroAcessoNegado())
+      return res.status(respostaHttp.status).json({ erro: respostaHttp.corpo.message })
     }
   }
 }
