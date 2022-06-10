13f0985182878f79e1441cb9a7a91ee390952653
Felipe Pedrosa <marconizero@gmail.com>
2022-06-10 14:55:16 -0300

    teste: garante que a rota de cadastro de equipamentos retorna status 200 caso um token valido seja fornecido

--- src/principal/rotas/rotas-equipamentos.test.ts
+++ src/principal/rotas/rotas-equipamentos.test.ts
@@ -3,6 +3,8 @@ import app from '../config/app'
 import { AuxiliaresMariaDB } from '../../infraestrutura/bd/mariadb/auxiliares/auxiliar-mariadb'
 import { Equipamento } from '../../infraestrutura/bd/mariadb/models/modelo-equipamento'
 import { Funcionario } from '../../infraestrutura/bd/mariadb/models/modelo-funcionarios'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 import 'dotenv/config'
 
 describe('Rotas equipamentos', () => {
@@ -46,5 +48,29 @@ describe('Rotas equipamentos', () => {
         estacaoId: '1'
       })
       .expect(403)
+  })
+
+  test('Deve retornar um status 200 ao adicionar um equipamento com um token válido', async () => {
+    const senha = await hash('123', 12)
+    const resposta = await Funcionario.create({
+      nome: 'alguém',
+      email: 'email@email.com',
+      senha,
+      administrador: true,
+      areaId: 3
+    })
+    const chave_secreta = process.env.CHAVE_SECRETA //eslint-disable-line
+    const tokenDeAcesso = sign({ id: String(resposta.id) }, (chave_secreta as string), { expiresIn: 60 })
+    await request(app)
+      .post('/equipamento')
+      .set('authorization', `Bearer ${tokenDeAcesso}`)
+      .send({
+        nome: 'Escada rolante',
+        tipo: 'escada',
+        numFalha: '35647',
+        estado: '1',
+        estacaoId: '1'
+      })
+      .expect(200)
   })
 })
