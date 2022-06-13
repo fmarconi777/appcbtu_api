25b7244893e8f70a6f2b32ae4acc337674727d17
Felipe Pedrosa <marconizero@gmail.com>
2022-06-13 15:35:35 -0300

    funcionalidade: garante que a rota de cadastro de funcionario retorna status 403 ao tentar cadastrar funcionario sem autenticação

--- src/principal/rotas/equipamento-rotas.ts
+++ src/principal/rotas/equipamento-rotas.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { adaptadorDeMiddleware } from '../adaptadores/adaptador-de-middleware-express'
+import { criaMiddlewareDeAutenticacao } from '../fabrica/middleware-de-autenticacao'
 import { adaptadorDeRota } from '../adaptadores/adaptador-de-rota-express'
 import { criaControladorDeEquipamento } from '../fabrica/equipamento'
-import { criaMiddlewareDeAutenticacao } from '../fabrica/middleware-de-autenticacao'
 
 export default (router: Router): Router => {
   const autentificacaoArea = adaptadorDeMiddleware(criaMiddlewareDeAutenticacao('3'))
--- src/principal/rotas/funcionarios-rotas.ts
+++ src/principal/rotas/funcionarios-rotas.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { adaptadorDeMiddleware } from '../adaptadores/adaptador-de-middleware-express'
+import { criaMiddlewareDeAutenticacao } from '../fabrica/middleware-de-autenticacao'
 import { adaptadorDeRota } from '../adaptadores/adaptador-de-rota-express'
 import { criaControladorDeFuncionario } from '../fabrica/funcionario'
 import { criaControladorDeLogin } from '../fabrica/login'
 
 export default (router: Router): Router => {
-  router.post('/funcionario', adaptadorDeRota(criaControladorDeFuncionario())) // eslint-disable-line
+  const autentificacaoArea = adaptadorDeMiddleware(criaMiddlewareDeAutenticacao('admin'))
+  router.post('/funcionario', autentificacaoArea, adaptadorDeRota(criaControladorDeFuncionario())) // eslint-disable-line
   router.post('/login', adaptadorDeRota(criaControladorDeLogin())) // eslint-disable-line
   return router
 }
