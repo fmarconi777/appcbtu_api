b29bb9d7c39164c8cdfd2144d6c11d4e120090ab
Felipe Pedrosa <marconizero@gmail.com>
2022-06-14 14:22:34 -0300

    teste: garante que o controlador de area retorna status 200 e uma area se o parametro estiver correto

--- src/apresentacao/controladores/area.spec.ts
+++ src/apresentacao/controladores/area.spec.ts
@@ -62,11 +62,22 @@ describe('Controlador de estações', () => {
     }])
   })
 
-  test('Deve chamar ConsultaArea com o valor correto', async () => {
+  test('Deve chamar ConsultaEstacao com o valor correto', async () => {
     const { sut, consultaAreaStub } = makeSut()
     const spyConsula = jest.spyOn(consultaAreaStub, 'consultar')
     const requisicaoHttp = { parametro: 'area_qualquer', metodo: 'GET' }
     await sut.tratar(requisicaoHttp)
     expect(spyConsula).toHaveBeenCalledWith('area_qualquer')
+  })
+
+  test('Deve retornar codigo 200 e uma área se o parâmetro estiver correto', async () => {
+    const { sut } = makeSut()
+    const requisicaoHttp = { parametro: 'area_valida', metodo: 'GET' }
+    const respostaHttp = await sut.tratar(requisicaoHttp)
+    expect(respostaHttp.status).toBe(200)
+    expect(respostaHttp.corpo).toEqual({
+      id: 'id_valida',
+      nome: 'nome_valido'
+    })
   })
 })
