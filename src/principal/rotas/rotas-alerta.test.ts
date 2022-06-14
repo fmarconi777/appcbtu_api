9e0d9e55f0cb3c7e9ccfb3728a12c5bb523e30e4
Felipe Pedrosa <marconizero@gmail.com>
2022-06-14 10:59:34 -0300

    teste: garante que a rota de cadastro de alerta retorna status 403 ao tentar cadastrar um alerta com um token válido

--- src/principal/rotas/rotas-alerta.test.ts
+++ src/principal/rotas/rotas-alerta.test.ts
@@ -2,6 +2,10 @@ import request from 'supertest'
 import app from '../config/app'
 import { AuxiliaresMariaDB } from '../../infraestrutura/bd/mariadb/auxiliares/auxiliar-mariadb'
 import { Alerta } from '../../infraestrutura/bd/mariadb/models/modelo-alerta'
+import { Funcionario } from '../../infraestrutura/bd/mariadb/models/modelo-funcionarios'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import 'dotenv/config'
 
 describe('Rotas Alerta', () => {
   beforeAll(async () => {
@@ -45,5 +49,30 @@ describe('Rotas Alerta', () => {
         estacaoId: '1'
       })
       .expect(403)
+  })
+
+  test('Deve retornar status 200 ao adicionar um alerta com um token válido', async () => {
+    const senha = await hash('123', 12)
+    const resposta = await Funcionario.create({
+      nome: 'alguém',
+      email: 'email@email.com',
+      senha,
+      administrador: true,
+      areaId: 3
+    })
+    const chave_secreta = process.env.CHAVE_SECRETA //eslint-disable-line
+    const tokenDeAcesso = sign({ id: String(resposta.id) }, (chave_secreta as string), { expiresIn: 60 })
+    await request(app)
+      .post('/alerta')
+      .set('authorization', `Bearer ${tokenDeAcesso}`)
+      .send({
+        descricao: 'Estação Parada!',
+        prioridade: 'Altissima',
+        dataInicio: '2022-02-05',
+        dataFim: '2022-02-05',
+        ativo: 'true',
+        estacaoId: '1'
+      })
+      .expect(200)
   })
 })
