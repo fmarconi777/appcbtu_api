255a761d0c637cc179dfd424f7798404e6de3a14
Felipe Pedrosa <marconizero@gmail.com>
2022-06-14 10:56:12 -0300

    teste: garante que a rota de cadastro de alerta retorna status 403 ao tentar cadastrar um alerta com authorization sem token de acesso

--- src/principal/rotas/rotas-alerta.test.ts
+++ src/principal/rotas/rotas-alerta.test.ts
@@ -31,4 +31,19 @@ describe('Rotas Alerta', () => {
       })
       .expect(403)
   })
+
+  test('Deve retornar status 403 ao adicionar um alerta com authorization sem token de acesso', async () => {
+    await request(app)
+      .post('/alerta')
+      .set('authorization', 'Bearer ')
+      .send({
+        descricao: 'Estação Parada!',
+        prioridade: 'Altissima',
+        dataInicio: '2022-02-05',
+        dataFim: '2022-02-05',
+        ativo: 'true',
+        estacaoId: '1'
+      })
+      .expect(403)
+  })
 })
