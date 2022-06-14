b9a005e1f72b33da8ecf11dfe14a415c76bb6f31
Felipe Pedrosa <marconizero@gmail.com>
2022-06-14 09:54:35 -0300

    refatoracao: retirada a chamada direta ao bcrypt

--- src/principal/config/conta-administrador.ts
+++ src/principal/config/conta-administrador.ts
@@ -1,11 +1,11 @@
 import { AuxiliaresMariaDB } from '../../infraestrutura/bd/mariadb/auxiliares/auxiliar-mariadb'
-import { hash } from 'bcrypt'
-import 'dotenv/config'
+import { BcryptAdaptador } from '../../infraestrutura/criptografia/bcrypt-adaptador/bcrypt-adaptador'
 
 export default async function admin (): Promise<void> {
-  const salt = process.env.SALT ?? 12
-  const senha = await hash('123', +salt)
-  await AuxiliaresMariaDB.cliente?.query(`insert into Funcionario (nome, email, senha, administrador, areaId)
+  const bcryptAdaptador = new BcryptAdaptador()
+  const senha = await bcryptAdaptador.gerar('123')
+  const query = `insert into Funcionario (nome, email, senha, administrador, areaId)
   select * from (select 'admin' as nome, 'admin@email.com' as email, '${senha}' as senha, true as administrador, 9 as areaId) as admin
-  where not exists (select nome from Funcionario where nome = 'admin') limit 1;`)
+  where not exists (select nome from Funcionario where nome = 'admin') limit 1;`
+  await AuxiliaresMariaDB.cliente?.query(query)
 }
