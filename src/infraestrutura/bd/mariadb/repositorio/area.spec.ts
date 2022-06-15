45a4956304194bee57471b32c4620ba32b587cd2
Felipe Pedrosa <marconizero@gmail.com>
2022-06-15 11:37:55 -0300

    teste: garante que RepositorioAreaMariaDB retorna uma area se um parametro for fornecido

---
+++ src/infraestrutura/bd/mariadb/repositorio/area.spec.ts
@@ -0,0 +1,30 @@
+import { AuxiliaresMariaDB } from '../auxiliares/auxiliar-mariadb'
+import { RepositorioAreaMariaDB } from './area'
+
+describe('RepositorioAreaMariaDB', () => {
+  beforeAll(async () => {
+    await AuxiliaresMariaDB.conectar()
+    console.log('conexão aberta')
+  })
+
+  afterAll(async () => {
+    await AuxiliaresMariaDB.desconectar()
+    console.log('conexão fechada')
+  })
+
+  const makeSut = (): RepositorioAreaMariaDB => {
+    return new RepositorioAreaMariaDB()
+  }
+
+  const resultadoEsperado = {
+    nome: 'COINF'
+  }
+
+  test('Deve retornar uma área se um parametro for fornecido', async () => {
+    const sut = makeSut()
+    const resposta = await sut.consultar('COINF')
+    expect(resposta).toBeTruthy()
+    expect(resposta.id).toBeTruthy()
+    expect(resposta).toMatchObject(resultadoEsperado)
+  })
+})
