85198b32f40f54a600fd6f7dd4811265c0157cde
Felipe Pedrosa <marconizero@gmail.com>
2022-06-15 15:20:52 -0300

    teste: garante que a rota area retorna status 403 ao consultar uma area sem autenticação

---
+++ src/principal/rotas/rotas-area.test.ts
@@ -0,0 +1,24 @@
+import { AuxiliaresMariaDB } from '../../infraestrutura/bd/mariadb/auxiliares/auxiliar-mariadb'
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Rotas Area', () => {
+  beforeAll(async () => {
+    await AuxiliaresMariaDB.conectar()
+    console.log('conexão aberta')
+  })
+
+  afterAll(async () => {
+    await AuxiliaresMariaDB.desconectar()
+    console.log('conexão fechada')
+  })
+
+  describe('GET', () => {
+    test('Deve retornar status 403 ao consultar uma area sem autenticação', async () => {
+      await request(app)
+        .get('/area')
+        .send()
+        .expect(403)
+    })
+  })
+})
