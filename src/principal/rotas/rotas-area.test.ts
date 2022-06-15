2d824d13b5462dfda2b76a50ea58c103f25e402b
Felipe Pedrosa <marconizero@gmail.com>
2022-06-15 15:24:59 -0300

    teste: garante que a rota area retorna status 200 ao consultar uma area com token de acesso válido

--- src/principal/rotas/rotas-area.test.ts
+++ src/principal/rotas/rotas-area.test.ts
@@ -1,6 +1,9 @@
 import { AuxiliaresMariaDB } from '../../infraestrutura/bd/mariadb/auxiliares/auxiliar-mariadb'
 import request from 'supertest'
 import app from '../config/app'
+import { hash } from 'bcrypt'
+import { Funcionario } from '../../infraestrutura/bd/mariadb/models/modelo-funcionarios'
+import { sign } from 'jsonwebtoken'
 
 describe('Rotas Area', () => {
   beforeAll(async () => {
@@ -27,6 +30,24 @@ describe('Rotas Area', () => {
         .set('authorization', 'Bearer ')
         .send()
         .expect(403)
+    })
+
+    test('Deve retornar status 200 ao consultar uma area com token de acesso válido', async () => {
+      const senha = await hash('123', 12)
+      const resposta = await Funcionario.create({
+        nome: 'alguém',
+        email: 'email@email.com',
+        senha,
+        administrador: true,
+        areaId: 3
+      })
+      const chave_secreta = process.env.CHAVE_SECRETA //eslint-disable-line
+      const tokenDeAcesso = sign({ id: String(resposta.id) }, (chave_secreta as string), { expiresIn: 60 })
+      await request(app)
+        .get('/area')
+        .set('authorization', `Bearer ${tokenDeAcesso}`)
+        .send()
+        .expect(200)
     })
   })
 })
