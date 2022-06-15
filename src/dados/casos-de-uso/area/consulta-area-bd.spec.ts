475bb0afce6534fe96c92d0eeeaa46f2209a5f8a
Felipe Pedrosa <marconizero@gmail.com>
2022-06-15 08:24:19 -0300

    teste: garante que o ConsultaAreaBD chama o RepositorioArea com o valor correto

---
+++ src/dados/casos-de-uso/area/consulta-area-bd.spec.ts
@@ -0,0 +1,42 @@
+import { ConsultaAreaBD } from './consulta-area-bd'
+import { RepositorioArea, ModelosAreas } from '../../protocolos/bd/repositorio-area'
+
+const makeRepositorioArea = (): RepositorioArea => {
+  class RepositorioAreaStub implements RepositorioArea {
+    async consultar (area?: string): Promise<ModelosAreas> {
+      const areaFalsa = {
+        id: 'id_qualquer',
+        nome: 'AREA_QUALQUER'
+      }
+      if (area) { //eslint-disable-line
+        return await new Promise(resolve => resolve(areaFalsa))
+      }
+      return await new Promise(resolve => resolve([areaFalsa]))
+    }
+  }
+  return new RepositorioAreaStub()
+}
+
+interface SubTipo {
+  sut: ConsultaAreaBD
+  repositorioAreaStub: RepositorioArea
+}
+
+const makeSut = (): SubTipo => {
+  const repositorioAreaStub = makeRepositorioArea()
+  const sut = new ConsultaAreaBD(repositorioAreaStub)
+  return {
+    sut,
+    repositorioAreaStub
+  }
+}
+
+describe('ConsultaAreaBD', () => {
+  test('Deve chamar o RepositorioArea com o valor correto', async () => {
+    const { sut, repositorioAreaStub } = makeSut()
+    const consultarSpy = jest.spyOn(repositorioAreaStub, 'consultar')
+    const area = 'AREA_QUALQUER'
+    await sut.consultar(area)
+    expect(consultarSpy).toHaveBeenCalledWith('AREA_QUALQUER')
+  })
+})
