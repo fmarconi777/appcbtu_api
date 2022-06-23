338eb5fbf0689cf459fe3a29d05dbbae461ba1cb
Felipe Pedrosa <marconizero@gmail.com>
2022-06-23 10:39:14 -0300

    funcionalidade: garante que o RepositorioAreaMariaDB retorna uma área em caso de sucesso

--- src/infraestrutura/bd/mariadb/repositorio/area.ts
+++ src/infraestrutura/bd/mariadb/repositorio/area.ts
@@ -1,11 +1,12 @@
 import { RepositorioArea, ModelosAreas } from '../../../../dados/protocolos/bd/area/repositorio-area'
 import { ConsultaAreaPorNome } from '../../../../dados/protocolos/bd/area/repositorio-consulta-area-por-nome'
+import { RepositorioInserirArea } from '../../../../dados/protocolos/bd/area/repositorio-inserir-area'
 import { ModeloArea } from '../../../../dominio/modelos/area'
 import { AuxiliaresMariaDB } from '../auxiliares/auxiliar-mariadb'
 import { FuncoesAuxiliares } from '../auxiliares/funcoes-auxiliares'
 import { Area } from '../models/modelo-area'
 
-export class RepositorioAreaMariaDB implements RepositorioArea, ConsultaAreaPorNome {
+export class RepositorioAreaMariaDB implements RepositorioArea, ConsultaAreaPorNome, RepositorioInserirArea {
   async consultar (area?: string | undefined): Promise<ModelosAreas> {
     AuxiliaresMariaDB.verificaConexao()
     if (area) { //eslint-disable-line
@@ -18,5 +19,11 @@ export class RepositorioAreaMariaDB implements RepositorioArea, ConsultaAreaPorN
     AuxiliaresMariaDB.verificaConexao()
     const area = await Area.findOne({ where: { nome: nome } })
     return area ? FuncoesAuxiliares.mapeadorDeDados(area) : null //eslint-disable-line
+  }
+
+  async inserir (nome: string): Promise<ModeloArea> {
+    AuxiliaresMariaDB.verificaConexao()
+    const area = await Area.create({ nome })
+    return FuncoesAuxiliares.mapeadorDeDados(area)
   }
 }
