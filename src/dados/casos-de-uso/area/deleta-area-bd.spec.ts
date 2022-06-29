38e93b0432d4a8f844847998691778f7b4bd3cff
Felipe Pedrosa <marconizero@gmail.com>
2022-06-29 11:15:01 -0300

    teste: garante que o DeletaAreaBD chama o RepositorioArea com o valor correto

---
+++ src/dados/casos-de-uso/area/deleta-area-bd.spec.ts
@@ -0,0 +1,34 @@
+import { RepositorioDeletaArea } from '../../protocolos/bd/area/repositorio-deleta-area'
+import { DeletaAreaBD } from './deleta-area-bd'
+
+const makeRepositorioArea = (): RepositorioDeletaArea => {
+  class RepositorioDeletaAreaStub implements RepositorioDeletaArea {
+    async deletar (area: string): Promise<string> {
+      return await new Promise(resolve => resolve('Área deletada com sucesso'))
+    }
+  }
+  return new RepositorioDeletaAreaStub()
+}
+
+interface SubTipo {
+  sut: DeletaAreaBD
+  repositorioDeletaAreaStub: RepositorioDeletaArea
+}
+
+const makeSut = (): SubTipo => {
+  const repositorioDeletaAreaStub = makeRepositorioArea()
+  const sut = new DeletaAreaBD(repositorioDeletaAreaStub)
+  return {
+    sut,
+    repositorioDeletaAreaStub
+  }
+}
+
+describe('DeletaAreaBD', () => {
+  test('Deve chamar o RepositorioArea com o valor correto', async () => {
+    const { sut, repositorioDeletaAreaStub } = makeSut()
+    const deletarSpy = jest.spyOn(repositorioDeletaAreaStub, 'deletar')
+    await sut.deletar('AREA_QUALQUER')
+    expect(deletarSpy).toHaveBeenCalledWith('AREA_QUALQUER')
+  })
+})
