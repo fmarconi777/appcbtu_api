a68f70131d2c204cbb66325518bc40daa9496734
Felipe Pedrosa <marconizero@gmail.com>
2022-06-30 10:34:39 -0300

    funcionalidade: garante que RepositorioAreaMariaDB retorna a mensagem 'Erro ao deletar área' em caso de falha

--- src/infraestrutura/bd/mariadb/repositorio/area.ts
+++ src/infraestrutura/bd/mariadb/repositorio/area.ts
@@ -1,12 +1,13 @@
 import { RepositorioArea, ModelosAreas } from '../../../../dados/protocolos/bd/area/repositorio-area'
 import { ConsultaAreaPorNome } from '../../../../dados/protocolos/bd/area/repositorio-consulta-area-por-nome'
+import { RepositorioDeletaArea } from '../../../../dados/protocolos/bd/area/repositorio-deleta-area'
 import { RepositorioInserirArea } from '../../../../dados/protocolos/bd/area/repositorio-inserir-area'
 import { ModeloArea } from '../../../../dominio/modelos/area'
 import { AuxiliaresMariaDB } from '../auxiliares/auxiliar-mariadb'
 import { FuncoesAuxiliares } from '../auxiliares/funcoes-auxiliares'
 import { Area } from '../models/modelo-area'
 
-export class RepositorioAreaMariaDB implements RepositorioArea, ConsultaAreaPorNome, RepositorioInserirArea {
+export class RepositorioAreaMariaDB implements RepositorioArea, ConsultaAreaPorNome, RepositorioInserirArea, RepositorioDeletaArea {
   async consultar (area?: string | undefined): Promise<ModelosAreas> {
     AuxiliaresMariaDB.verificaConexao()
     if (area) { //eslint-disable-line
@@ -25,5 +26,14 @@ export class RepositorioAreaMariaDB implements RepositorioArea, ConsultaAreaPorN
     AuxiliaresMariaDB.verificaConexao()
     const area = await Area.create({ nome })
     return FuncoesAuxiliares.mapeadorDeDados(area)
+  }
+
+  async deletar (nome: string): Promise<string> {
+    AuxiliaresMariaDB.verificaConexao()
+    const areaDeletada = await Area.destroy({ where: { nome } })
+    if (areaDeletada === 0) {
+      return 'Erro ao deletar área'
+    }
+    return await new Promise(resolve => resolve(''))
   }
 }
