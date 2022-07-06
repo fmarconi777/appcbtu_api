d4b8e64c3a15af6cbbacbd08bbe8a067596c0c76
Felipe Pedrosa <marconizero@gmail.com>
2022-07-06 17:41:10 -0300

    funcionalidade: garante o ControladorDeEquipamento retorna status 404 caso o parametro seja inválido

--- src/apresentacao/controladores/equipamento.ts
+++ src/apresentacao/controladores/equipamento.ts
@@ -1,10 +1,11 @@
-import { erroDeServidor, requisicaoImpropria, resposta } from '../auxiliares/auxiliar-http'
+import { erroDeServidor, requisicaoImpropria, requisicaoNaoEncontrada, resposta } from '../auxiliares/auxiliar-http'
 import { Controlador } from '../protocolos/controlador'
 import { RequisicaoHttp, RespostaHttp } from '../protocolos/http'
 import { ErroFaltaParametro } from '../erros/erro-falta-parametro'
 import { CadastroDeEquipamento } from '../../dominio/casos-de-uso/equipamento/cadastro-de-equipamento'
 import { ErroMetodoInvalido } from '../erros/erro-metodo-invalido'
 import { ConsultaEquipamento } from '../../dominio/casos-de-uso/equipamento/consulta-equipamento'
+import { ErroParametroInvalido } from '../erros/erro-parametro-invalido'
 
 export class ControladorDeEquipamento implements Controlador {
   constructor (
@@ -21,6 +22,9 @@ export class ControladorDeEquipamento implements Controlador {
           if (!parametro) { // eslint-disable-line
             const todosEquipamentos = await this.consultaEquipamento.consultarTodos()
             return resposta(todosEquipamentos)
+          }
+          if (!Number.isInteger(+parametro) || +parametro !== Math.abs(+parametro)) {
+            return requisicaoNaoEncontrada(new ErroParametroInvalido('id'))
           }
           const equipamento = await this.consultaEquipamento.consultar(+parametro)
           return resposta(equipamento)
