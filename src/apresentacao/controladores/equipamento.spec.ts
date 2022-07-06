e71c2e6c2f41997f8b4b2e01221e711481c317b8
Felipe Pedrosa <marconizero@gmail.com>
2022-07-06 14:58:22 -0300

    teste: garante o ControladorDeEquipamento retorna todos os equipamentos caso um parametro não seja fornecido

--- src/apresentacao/controladores/equipamento.spec.ts
+++ src/apresentacao/controladores/equipamento.spec.ts
@@ -4,6 +4,7 @@ import { CadastroDeEquipamento, DadosEquipamento } from '../../dominio/casos-de-
 import { ModeloEquipamento } from '../../dominio/modelos/equipamento'
 import { ErroDeServidor } from '../erros/erro-de-servidor'
 import { ErroMetodoInvalido } from '../erros/erro-metodo-invalido'
+import { ConsultaEquipamento } from '../../dominio/casos-de-uso/equipamento/consulta-equipamento'
 
 const makeCadastroDeEquipamento = (): CadastroDeEquipamento => {
   class CadastroDeEquipamentoStub implements CadastroDeEquipamento {
@@ -22,162 +23,46 @@ const makeCadastroDeEquipamento = (): CadastroDeEquipamento => {
   return new CadastroDeEquipamentoStub()
 }
 
+const dadosFalsos = {
+  id: 'id_qualquer',
+  nome: 'nome_qualquer',
+  tipo: 'tipo_qualquer',
+  numFalha: 'numFalha_qualquer',
+  estado: 'estado_qualquer',
+  estacaoId: 'estacaoId_qualquer'
+}
+
+const makeConsultaEquipamentoStub = (): ConsultaEquipamento => {
+  class ConsultaEquipamentoStub implements ConsultaEquipamento {
+    async consultarTodos (): Promise<ModeloEquipamento[]> {
+      return await new Promise(resolve => resolve([dadosFalsos]))
+    }
+
+    async consultar (id: number): Promise<ModeloEquipamento | string> {
+      return await new Promise(resolve => resolve(dadosFalsos))
+    }
+  }
+  return new ConsultaEquipamentoStub()
+}
+
 interface SutTypes {
   sut: ControladorDeEquipamento
   cadastroDeEquipamentoStub: CadastroDeEquipamento
+  consultaEquipamentoStub: ConsultaEquipamento
 }
 
 const makeSut = (): SutTypes => {
+  const consultaEquipamentoStub = makeConsultaEquipamentoStub()
   const cadastroDeEquipamentoStub = makeCadastroDeEquipamento()
-  const sut = new ControladorDeEquipamento(cadastroDeEquipamentoStub)
+  const sut = new ControladorDeEquipamento(cadastroDeEquipamentoStub, consultaEquipamentoStub)
   return {
     sut,
-    cadastroDeEquipamentoStub
+    cadastroDeEquipamentoStub,
+    consultaEquipamentoStub
   }
 }
 
 describe('Controlador de equipamentos', () => {
-  test('Deve retornar codigo 400 se um nome não for fornecido', async () => {
-    const { sut } = makeSut()
-    const requisicaoHttp = {
-      corpo: {
-        tipo: 'qualquer_tipo',
-        numFalha: 'numFalha_qualquer',
-        estado: 'estado_qualquer',
-        estacaoId: 'estacaoId_qualquer'
-      },
-      metodo: 'POST'
-    }
-    const respostaHttp = await sut.tratar(requisicaoHttp)
-    expect(respostaHttp.status).toBe(400)
-    expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('nome'))
-  })
-  test('Deve retornar codigo 400 se um tipo não for fornecido', async () => {
-    const { sut } = makeSut()
-    const requisicaoHttp = {
-      corpo: {
-        nome: 'qualquer_tipo',
-        numFalha: 'numFalha_qualquer',
-        estado: 'estado_qualquer',
-        estacaoId: 'estacaoId_qualquer'
-      },
-      metodo: 'POST'
-    }
-    const respostaHttp = await sut.tratar(requisicaoHttp)
-    expect(respostaHttp.status).toBe(400)
-    expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('tipo'))
-  })
-  test('Deve retornar codigo 400 se um numFalha não for fornecido', async () => {
-    const { sut } = makeSut()
-    const requisicaoHttp = {
-      corpo: {
-        nome: 'qualquer_nome',
-        tipo: 'qualquer_tipo',
-        estado: 'estado_qualquer',
-        estacaoId: 'estacaoId_qualquer'
-      },
-      metodo: 'POST'
-    }
-    const respostaHttp = await sut.tratar(requisicaoHttp)
-    expect(respostaHttp.status).toBe(400)
-    expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('numFalha'))
-  })
-  test('Deve retornar codigo 400 se um estado não for fornecido', async () => {
-    const { sut } = makeSut()
-    const requisicaoHttp = {
-      corpo: {
-        nome: 'qualquer_nome',
-        tipo: 'qualquer_tipo',
-        numFalha: 'numFalha_qualquer',
-        estacaoId: 'estacaoId_qualquer'
-      },
-      metodo: 'POST'
-    }
-    const respostaHttp = await sut.tratar(requisicaoHttp)
-    expect(respostaHttp.status).toBe(400)
-    expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('estado'))
-  })
-  test('Deve retornar codigo 400 se um estacaoId não for fornecido', async () => {
-    const { sut } = makeSut()
-    const requisicaoHttp = {
-      corpo: {
-        nome: 'qualquer_nome',
-        tipo: 'qualquer_tipo',
-        numFalha: 'numFalha_qualquer',
-        estado: 'estado_qualquer'
-      },
-      metodo: 'POST'
-    }
-    const respostaHttp = await sut.tratar(requisicaoHttp)
-    expect(respostaHttp.status).toBe(400)
-    expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('estacaoId'))
-  })
-  test('Deve chamar o CadastroDeEquipamento com os valores corretos', async () => {
-    const { sut, cadastroDeEquipamentoStub } = makeSut()
-    const inserirSpy = jest.spyOn(cadastroDeEquipamentoStub, 'inserir')
-    const requisicaoHttp = {
-      corpo: {
-        nome: 'qualquer_nome',
-        tipo: 'qualquer_tipo',
-        numFalha: 'numFalha_qualquer',
-        estado: 'estado_qualquer',
-        estacaoId: 'estacaoId_qualquer'
-      },
-      metodo: 'POST'
-    }
-    await sut.tratar(requisicaoHttp)
-    expect(inserirSpy).toHaveBeenCalledWith({
-      nome: 'qualquer_nome',
-      tipo: 'qualquer_tipo',
-      numFalha: 'numFalha_qualquer',
-      estado: 'estado_qualquer',
-      estacaoId: 'estacaoId_qualquer'
-    })
-  })
-  test('Deve retornar codigoo 500 se o CadastroDeEquipamentos retornar um erro', async () => {
-    const { sut, cadastroDeEquipamentoStub } = makeSut()
-    const erroFalso = new Error()
-    erroFalso.stack = 'stack_qualquer'
-    jest.spyOn(cadastroDeEquipamentoStub, 'inserir').mockImplementationOnce(async () => {
-      return await new Promise((resolve, reject) => reject(erroFalso))
-    })
-    const requisicaoHttp = {
-      corpo: {
-        nome: 'qualquer_nome',
-        tipo: 'qualquer_tipo',
-        numFalha: 'numFalha_qualquer',
-        estado: 'estado_qualquer',
-        estacaoId: 'estacaoId_qualquer'
-      },
-      metodo: 'POST'
-    }
-    const respostaHttp = await sut.tratar(requisicaoHttp)
-    expect(respostaHttp.status).toBe(500)
-    expect(respostaHttp.corpo).toEqual(new ErroDeServidor(erroFalso.stack))
-  })
-  test('Deve retornar codigoo 200 se dados válidos forem passados', async () => {
-    const { sut } = makeSut()
-    const requisicaoHttp = {
-      corpo: {
-        nome: 'qualquer_nome',
-        tipo: 'qualquer_tipo',
-        numFalha: 'numFalha_qualquer',
-        estado: 'estado_qualquer',
-        estacaoId: 'estacaoId_qualquer'
-      },
-      metodo: 'POST'
-    }
-    const respostaHttp = await sut.tratar(requisicaoHttp)
-    expect(respostaHttp.status).toBe(200)
-    expect(respostaHttp.corpo).toEqual({
-      id: 'qualquer_id',
-      nome: 'qualquer_nome',
-      tipo: 'qualquer_tipo',
-      numFalha: 'numFalha_qualquer',
-      estado: 'estado_qualquer',
-      estacaoId: 'estacaoId_qualquer'
-    })
-  })
   test('Deve retornar codigo 400 se um método não suportado for fornecido', async () => {
     const { sut } = makeSut()
     const requisicaoHttp = {
@@ -193,5 +78,159 @@ describe('Controlador de equipamentos', () => {
     const respostaHttp = await sut.tratar(requisicaoHttp)
     expect(respostaHttp.status).toBe(400)
     expect(respostaHttp.corpo).toEqual(new ErroMetodoInvalido())
+  })
+
+  describe('Método POST', () => {
+    test('Deve retornar codigo 400 se um nome não for fornecido', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = {
+        corpo: {
+          tipo: 'qualquer_tipo',
+          numFalha: 'numFalha_qualquer',
+          estado: 'estado_qualquer',
+          estacaoId: 'estacaoId_qualquer'
+        },
+        metodo: 'POST'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(400)
+      expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('nome'))
+    })
+    test('Deve retornar codigo 400 se um tipo não for fornecido', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = {
+        corpo: {
+          nome: 'qualquer_tipo',
+          numFalha: 'numFalha_qualquer',
+          estado: 'estado_qualquer',
+          estacaoId: 'estacaoId_qualquer'
+        },
+        metodo: 'POST'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(400)
+      expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('tipo'))
+    })
+    test('Deve retornar codigo 400 se um numFalha não for fornecido', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = {
+        corpo: {
+          nome: 'qualquer_nome',
+          tipo: 'qualquer_tipo',
+          estado: 'estado_qualquer',
+          estacaoId: 'estacaoId_qualquer'
+        },
+        metodo: 'POST'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(400)
+      expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('numFalha'))
+    })
+    test('Deve retornar codigo 400 se um estado não for fornecido', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = {
+        corpo: {
+          nome: 'qualquer_nome',
+          tipo: 'qualquer_tipo',
+          numFalha: 'numFalha_qualquer',
+          estacaoId: 'estacaoId_qualquer'
+        },
+        metodo: 'POST'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(400)
+      expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('estado'))
+    })
+    test('Deve retornar codigo 400 se um estacaoId não for fornecido', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = {
+        corpo: {
+          nome: 'qualquer_nome',
+          tipo: 'qualquer_tipo',
+          numFalha: 'numFalha_qualquer',
+          estado: 'estado_qualquer'
+        },
+        metodo: 'POST'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(400)
+      expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('estacaoId'))
+    })
+    test('Deve chamar o CadastroDeEquipamento com os valores corretos', async () => {
+      const { sut, cadastroDeEquipamentoStub } = makeSut()
+      const inserirSpy = jest.spyOn(cadastroDeEquipamentoStub, 'inserir')
+      const requisicaoHttp = {
+        corpo: {
+          nome: 'qualquer_nome',
+          tipo: 'qualquer_tipo',
+          numFalha: 'numFalha_qualquer',
+          estado: 'estado_qualquer',
+          estacaoId: 'estacaoId_qualquer'
+        },
+        metodo: 'POST'
+      }
+      await sut.tratar(requisicaoHttp)
+      expect(inserirSpy).toHaveBeenCalledWith({
+        nome: 'qualquer_nome',
+        tipo: 'qualquer_tipo',
+        numFalha: 'numFalha_qualquer',
+        estado: 'estado_qualquer',
+        estacaoId: 'estacaoId_qualquer'
+      })
+    })
+    test('Deve retornar codigoo 500 se o CadastroDeEquipamentos retornar um erro', async () => {
+      const { sut, cadastroDeEquipamentoStub } = makeSut()
+      const erroFalso = new Error()
+      erroFalso.stack = 'stack_qualquer'
+      jest.spyOn(cadastroDeEquipamentoStub, 'inserir').mockImplementationOnce(async () => {
+        return await new Promise((resolve, reject) => reject(erroFalso))
+      })
+      const requisicaoHttp = {
+        corpo: {
+          nome: 'qualquer_nome',
+          tipo: 'qualquer_tipo',
+          numFalha: 'numFalha_qualquer',
+          estado: 'estado_qualquer',
+          estacaoId: 'estacaoId_qualquer'
+        },
+        metodo: 'POST'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(500)
+      expect(respostaHttp.corpo).toEqual(new ErroDeServidor(erroFalso.stack))
+    })
+    test('Deve retornar codigoo 200 se dados válidos forem passados', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = {
+        corpo: {
+          nome: 'qualquer_nome',
+          tipo: 'qualquer_tipo',
+          numFalha: 'numFalha_qualquer',
+          estado: 'estado_qualquer',
+          estacaoId: 'estacaoId_qualquer'
+        },
+        metodo: 'POST'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(200)
+      expect(respostaHttp.corpo).toEqual({
+        id: 'qualquer_id',
+        nome: 'qualquer_nome',
+        tipo: 'qualquer_tipo',
+        numFalha: 'numFalha_qualquer',
+        estado: 'estado_qualquer',
+        estacaoId: 'estacaoId_qualquer'
+      })
+    })
+  })
+
+  describe('Método GET', () => {
+    test('Deve retornar todos os equipamentos caso um parametro não seja fornecido', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = { metodo: 'GET' }
+      const resposta = await sut.tratar(requisicaoHttp)
+      expect(resposta.status).toBe(200)
+      expect(resposta.corpo).toEqual([dadosFalsos])
+    })
   })
 })
