6f0b37d32364e9e96637db3e3bd7a7b2e6153344
Felipe Pedrosa <marconizero@gmail.com>
2022-07-07 16:05:20 -0300

    teste: garante que o RepositorioEquipamentoMariaDB retorna um array de equipamentos em caso de sucesso se um parametro não for fornecido

--- src/infraestrutura/bd/mariadb/repositorio/equipamento.spec.ts
+++ src/infraestrutura/bd/mariadb/repositorio/equipamento.spec.ts
@@ -17,21 +17,40 @@ describe('Repositorio mariaDB Equipamento', () => {
     await Equipamento.destroy({ truncate: true, cascade: false })
   })
 
-  test('Deve retornar um equipamento em caso de sucesso', async () => {
-    const sut = new RepositorioEquipamentoMariaDB()
-    const equipamento = await sut.inserir({
-      nome: 'nome_valido',
-      tipo: 'tipo_valido',
-      numFalha: '1',
-      estado: '1',
-      estacaoId: '1'
+  describe('Método inserir', () => {
+    test('Deve retornar um equipamento em caso de sucesso', async () => {
+      const sut = new RepositorioEquipamentoMariaDB()
+      const equipamento = await sut.inserir({
+        nome: 'nome_valido',
+        tipo: 'tipo_valido',
+        numFalha: '1',
+        estado: '1',
+        estacaoId: '1'
+      })
+      expect(equipamento).toBeTruthy()
+      expect(equipamento.id).toBeTruthy()
+      expect(equipamento.nome).toBe('nome_valido')
+      expect(equipamento.tipo).toBe('tipo_valido')
+      expect(equipamento.numFalha).toBe('1')
+      expect(equipamento.estado).toBe('1')
+      expect(equipamento.estacaoId).toBe('1')
     })
-    expect(equipamento).toBeTruthy()
-    expect(equipamento.id).toBeTruthy()
-    expect(equipamento.nome).toBe('nome_valido')
-    expect(equipamento.tipo).toBe('tipo_valido')
-    expect(equipamento.numFalha).toBe('1')
-    expect(equipamento.estado).toBe('1')
-    expect(equipamento.estacaoId).toBe('1')
+  })
+
+  describe('Método consultar', () => {
+    test('Deve retornar um array de equipamentos em caso de sucesso se um parametro não for fornecido', async () => {
+      const sut = new RepositorioEquipamentoMariaDB()
+      await sut.inserir({
+        nome: 'nome_valido',
+        tipo: 'tipo_valido',
+        numFalha: '1',
+        estado: '1',
+        estacaoId: '1'
+      })
+      const equipamentos: any = await sut.consultar()
+      expect(equipamentos).toBeTruthy()
+      expect(Array.isArray(equipamentos)).toBeTruthy()
+      expect(equipamentos.length).toBeGreaterThan(0)
+    })
   })
 })
