390b34e9a18337e58af8f94ead7213f5236f16bd
Felipe Pedrosa <marconizero@gmail.com>
2022-07-07 16:05:08 -0300

    funcionalidade: garante que o RepositorioEquipamentoMariaDB retorna um array de equipamentos em caso de sucesso se um parametro não for fornecido

--- src/infraestrutura/bd/mariadb/repositorio/equipamento.ts
+++ src/infraestrutura/bd/mariadb/repositorio/equipamento.ts
@@ -2,12 +2,17 @@ import { RepositorioEquipamento, DadosEquipamento, ModeloEquipamento } from '../
 import { Equipamento } from '../models/modelo-equipamento'
 import { FuncoesAuxiliares } from '../auxiliares/funcoes-auxiliares'
 import { AuxiliaresMariaDB } from '../auxiliares/auxiliar-mariadb'
+import { RepositorioConsultaEquipamento } from '../../../../dados/protocolos/bd/equipamento/repositorio-consulta-equipamento'
 
-export class RepositorioEquipamentoMariaDB implements RepositorioEquipamento {
+export class RepositorioEquipamentoMariaDB implements RepositorioEquipamento, RepositorioConsultaEquipamento {
   async inserir (dadosEquipamento: DadosEquipamento): Promise<ModeloEquipamento> {
     AuxiliaresMariaDB.verificaConexao()
     const equipamento = await Equipamento.create(this.transformaDados(dadosEquipamento))
     return FuncoesAuxiliares.mapeadorDeDados(equipamento)
+  }
+
+  async consultar (id?: number | undefined): Promise<ModeloEquipamento | ModeloEquipamento[] | any> {
+    return await Equipamento.findAll()
   }
 
   private transformaDados (dadosEquipamento: DadosEquipamento): any {
