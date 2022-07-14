ca76b4a1ffd17a9493fdf37664a5a9edb8f74acd
Felipe Pedrosa <marconizero@gmail.com>
2022-07-14 09:30:25 -0300

    refatoracao: adiciona validaEstacao ao criaControladorDeEquipamento

--- src/principal/fabrica/equipamento.ts
+++ src/principal/fabrica/equipamento.ts
@@ -5,12 +5,18 @@ import { RepositorioLogDeErroMariaDB } from '../../infraestrutura/bd/mariadb/rep
 import { Controlador } from '../../apresentacao/protocolos/controlador'
 import { DecoradorControladorLog } from '../decoradores/log'
 import { ConsultaEquipamentoBD } from '../../dados/casos-de-uso/equipamento/consulta-equipamento-bd'
+import { ValidadorDeEstacao } from '../../utilidades/validadores/validador-de-estacao'
+import { ConsultaEstacaoBD } from '../../dados/casos-de-uso/estacao/consulta-estacao-bd'
+import { RepositorioEstacaoMariaDB } from '../../infraestrutura/bd/mariadb/repositorio/estacao'
 
 export const criaControladorDeEquipamento = (): Controlador => {
   const repositorioEquipamento = new RepositorioEquipamentoMariaDB()
   const cadastroDeEquipamento = new CadastroDeEquipamentoBd(repositorioEquipamento)
   const consultaEquipamentoBD = new ConsultaEquipamentoBD(repositorioEquipamento)
-  const controladorDeEquipamento = new ControladorDeEquipamento(cadastroDeEquipamento, consultaEquipamentoBD)
+  const repositorioEstacao = new RepositorioEstacaoMariaDB()
+  const consultaEstacao = new ConsultaEstacaoBD(repositorioEstacao)
+  const validaEstacao = new ValidadorDeEstacao(consultaEstacao)
+  const controladorDeEquipamento = new ControladorDeEquipamento(cadastroDeEquipamento, consultaEquipamentoBD, validaEstacao)
   const repositorioLogDeErro = new RepositorioLogDeErroMariaDB()
   return new DecoradorControladorLog(controladorDeEquipamento, repositorioLogDeErro)
 }
