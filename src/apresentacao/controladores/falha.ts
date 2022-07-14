c4110336994d03c6b2488e2bbe0042259aa0f678
Felipe Pedrosa <marconizero@gmail.com>
2022-07-14 11:20:24 -0300

    funcionalidade: garante que o ControladorDeFalha retorna codigo 500 caso o o validaEquipamento retorne um erro

--- src/apresentacao/controladores/falha.ts
+++ src/apresentacao/controladores/falha.ts
@@ -1,4 +1,4 @@
-import { requisicaoImpropria, resposta } from '../auxiliares/auxiliar-http'
+import { erroDeServidor, requisicaoImpropria, resposta } from '../auxiliares/auxiliar-http'
 import { ErroFaltaParametro } from '../erros/erro-falta-parametro'
 import { ErroMetodoInvalido } from '../erros/erro-metodo-invalido'
 import { Controlador } from '../protocolos/controlador'
@@ -12,16 +12,18 @@ export class ControladorDeFalha implements Controlador {
     const metodo = requisicaoHttp.metodo
     switch (metodo) {
       case 'POST':
-      {
-        const camposRequeridos = ['numFalha', 'equipamentoId']
-        for (const campo of camposRequeridos) {
-          if (!requisicaoHttp.corpo[campo]) { // eslint-disable-line
-            return requisicaoImpropria(new ErroFaltaParametro(campo))
+        try {
+          const camposRequeridos = ['numFalha', 'equipamentoId']
+          for (const campo of camposRequeridos) {
+            if (!requisicaoHttp.corpo[campo]) { // eslint-disable-line
+              return requisicaoImpropria(new ErroFaltaParametro(campo))
+            }
           }
+          await this.validaEquipamento.validar(+requisicaoHttp.corpo.equipamentoId)
+          return resposta('')
+        } catch (erro: any) {
+          return erroDeServidor(erro)
         }
-        await this.validaEquipamento.validar(+requisicaoHttp.corpo.equipamentoId)
-        return resposta('')
-      }
       default:
         return requisicaoImpropria(new ErroMetodoInvalido())
     }
