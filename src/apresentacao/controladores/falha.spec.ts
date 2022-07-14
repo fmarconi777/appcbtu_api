905eb8d5870c53a32c8cec82e5890c9c67a242b2
Felipe Pedrosa <marconizero@gmail.com>
2022-07-14 11:14:02 -0300

    teste: garante que o ControladorDeFalha chama o validaEquipamento com o valor correto

--- src/apresentacao/controladores/falha.spec.ts
+++ src/apresentacao/controladores/falha.spec.ts
@@ -1,21 +1,34 @@
 import { requisicaoImpropria } from '../auxiliares/auxiliar-http'
 import { ErroFaltaParametro } from '../erros/erro-falta-parametro'
 import { ErroMetodoInvalido } from '../erros/erro-metodo-invalido'
+import { ValidadorBD } from '../protocolos/validadorBD'
 import { ControladorDeFalha } from './falha'
+
+const makeValidaEquipamentoStub = (): ValidadorBD => {
+  class ValidaEquipamentoStub implements ValidadorBD {
+    async validar (parametro: any): Promise<boolean> {
+      return await new Promise(resolve => resolve(true))
+    }
+  }
+  return new ValidaEquipamentoStub()
+}
 
 const falhaFalsa = {
   numFalha: 'numero_qualquer',
-  equipamentoId: 'equipamentoId_qualquer'
+  equipamentoId: '1'
 }
 
 interface SubTipos {
   sut: ControladorDeFalha
+  validaEquipamentoStub: ValidadorBD
 }
 
 const makeSut = (): SubTipos => {
-  const sut = new ControladorDeFalha()
+  const validaEquipamentoStub = makeValidaEquipamentoStub()
+  const sut = new ControladorDeFalha(validaEquipamentoStub)
   return {
-    sut
+    sut,
+    validaEquipamentoStub
   }
 }
 
@@ -36,7 +49,7 @@ describe('ControladorDeFalha', () => {
       const { sut } = makeSut()
       const requisicaoHttp = {
         corpo: {
-          equipamentoId: 'equipamentoId_qualquer'
+          equipamentoId: '1'
         },
         metodo: 'POST'
       }
@@ -54,6 +67,17 @@ describe('ControladorDeFalha', () => {
       }
       const respostaHttp = await sut.tratar(requisicaoHttp)
       expect(respostaHttp).toEqual(requisicaoImpropria(new ErroFaltaParametro('equipamentoId')))
+    })
+
+    test('Deve chamar o validaEquipamento com o valor correto', async () => {
+      const { sut, validaEquipamentoStub } = makeSut()
+      const validarSpy = jest.spyOn(validaEquipamentoStub, 'validar')
+      const requisicaoHttp = {
+        corpo: falhaFalsa,
+        metodo: 'POST'
+      }
+      await sut.tratar(requisicaoHttp)
+      expect(validarSpy).toHaveBeenCalledWith(1)
     })
   })
 })
