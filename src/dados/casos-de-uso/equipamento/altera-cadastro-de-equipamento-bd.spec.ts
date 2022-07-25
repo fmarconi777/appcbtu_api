87ed35628646243fcacbadf8808b963ec3d57149
Felipe Pedrosa <marconizero@gmail.com>
2022-07-25 11:40:54 -0300

    teste: garante que AlteraCadastroDeEquipamentoBD chama o repositorioConsultaEquipamentoStub com o valor correto

--- src/dados/casos-de-uso/equipamento/altera-cadastro-de-equipamento-bd.spec.ts
+++ src/dados/casos-de-uso/equipamento/altera-cadastro-de-equipamento-bd.spec.ts
@@ -1,39 +1,62 @@
 import { ModeloEquipamento } from '../../../dominio/modelos/equipamento'
 import { RepositorioAlteraCadastroDeEquipamento } from '../../protocolos/bd/equipamento/repositorio-altera-cadastro-de-equipamento'
+import { RepositorioConsultaEquipamento } from '../../protocolos/bd/equipamento/repositorio-consulta-equipamento'
 import { AlteraCadastroDeEquipamentoBD } from './altera-cadastro-de-equipamento-bd'
 
 const dadosFalsos = {
-  id: 'id_qualquer',
+  id: '1',
   nome: 'nome_qualquer',
   tipo: 'tipo_qualquer',
   estado: 'estado_qualquer',
-  estacaoId: 'estacaoId_qualquer'
+  estacaoId: '1'
 }
 
 const makeRepositorioAlteraCadastroDeEquipamentoStub = (): RepositorioAlteraCadastroDeEquipamento => {
   class RepositorioAlteraCadastroDeEquipamentoStub implements RepositorioAlteraCadastroDeEquipamento {
-    async alterar (dadosEquipamento: ModeloEquipamento): Promise<string> {
+    async alterar (dadosEquipamento: ModeloEquipamento): Promise<string | null> {
       return await new Promise(resolve => resolve('Cadastro alterado com sucesso'))
     }
   }
   return new RepositorioAlteraCadastroDeEquipamentoStub()
 }
 
+const makeRepositorioConsultaEquipamento = (): RepositorioConsultaEquipamento => {
+  class RepositorioConsultaEquipamentoStub implements RepositorioConsultaEquipamento {
+    async consultar (id?: number): Promise<ModeloEquipamento | ModeloEquipamento[] | null> {
+      if (id) { // eslint-disable-line
+        return await new Promise(resolve => resolve(dadosFalsos))
+      }
+      return await new Promise(resolve => resolve([dadosFalsos]))
+    }
+  }
+  return new RepositorioConsultaEquipamentoStub()
+}
+
 interface SubTipos {
   sut: AlteraCadastroDeEquipamentoBD
   repositorioAlteraCadastroDeEquipamentoStub: RepositorioAlteraCadastroDeEquipamento
+  repositorioConsultaEquipamentoStub: RepositorioConsultaEquipamento
 }
 
 const makeSut = (): SubTipos => {
+  const repositorioConsultaEquipamentoStub = makeRepositorioConsultaEquipamento()
   const repositorioAlteraCadastroDeEquipamentoStub = makeRepositorioAlteraCadastroDeEquipamentoStub()
-  const sut = new AlteraCadastroDeEquipamentoBD(repositorioAlteraCadastroDeEquipamentoStub)
+  const sut = new AlteraCadastroDeEquipamentoBD(repositorioAlteraCadastroDeEquipamentoStub, repositorioConsultaEquipamentoStub)
   return {
     sut,
-    repositorioAlteraCadastroDeEquipamentoStub
+    repositorioAlteraCadastroDeEquipamentoStub,
+    repositorioConsultaEquipamentoStub
   }
 }
 
 describe('AlteraCadastroDeEquipamentoBD', () => {
+  test('Deve chamar o repositorioConsultaEquipamentoStub com o valor correto', async () => {
+    const { sut, repositorioConsultaEquipamentoStub } = makeSut()
+    const alterarSpy = jest.spyOn(repositorioConsultaEquipamentoStub, 'consultar')
+    await sut.alterar(dadosFalsos)
+    expect(alterarSpy).toHaveBeenCalledWith(+dadosFalsos.id)
+  })
+
   test('Deve chamar o repositorioAlteraCadastroDeEquipamentoStub com os valores corretos', async () => {
     const { sut, repositorioAlteraCadastroDeEquipamentoStub } = makeSut()
     const alterarSpy = jest.spyOn(repositorioAlteraCadastroDeEquipamentoStub, 'alterar')
