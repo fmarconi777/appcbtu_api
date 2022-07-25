ca04e1eec910f4bf6c0ecd4cb60262c78fababef
Felipe Pedrosa <marconizero@gmail.com>
2022-07-25 11:40:23 -0300

    funcionalidade: garante que AlteraCadastroDeEquipamentoBD chama o repositorioConsultaEquipamentoStub com o valor correto

--- src/dados/casos-de-uso/equipamento/altera-cadastro-de-equipamento-bd.ts
+++ src/dados/casos-de-uso/equipamento/altera-cadastro-de-equipamento-bd.ts
@@ -1,11 +1,16 @@
 import { AlteraCadastroDeEquipamento } from '../../../dominio/casos-de-uso/equipamento/altera-cadastro-de-equipamento'
 import { RepositorioAlteraCadastroDeEquipamento } from '../../protocolos/bd/equipamento/repositorio-altera-cadastro-de-equipamento'
+import { RepositorioConsultaEquipamento } from '../../protocolos/bd/equipamento/repositorio-consulta-equipamento'
 import { ModeloEquipamento } from '../../protocolos/bd/equipamento/repositorio-equipamento'
 
 export class AlteraCadastroDeEquipamentoBD implements AlteraCadastroDeEquipamento {
-  constructor (private readonly repositorioAlteraCadastroDeEquipamento: RepositorioAlteraCadastroDeEquipamento) {}
+  constructor (
+    private readonly repositorioAlteraCadastroDeEquipamento: RepositorioAlteraCadastroDeEquipamento,
+    private readonly repositorioConsultaEquipamento: RepositorioConsultaEquipamento
+  ) {}
 
-  async alterar (dadosEquipamento: ModeloEquipamento): Promise<string> {
+  async alterar (dadosEquipamento: ModeloEquipamento): Promise<string | null> {
+    await this.repositorioConsultaEquipamento.consultar(+dadosEquipamento.id)
     await this.repositorioAlteraCadastroDeEquipamento.alterar(dadosEquipamento)
     return await new Promise(resolve => resolve(''))
   }
