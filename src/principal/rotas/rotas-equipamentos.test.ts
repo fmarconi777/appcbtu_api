099f7263017e9082bdb7ebcd7a36655df600a7be
Felipe Pedrosa <marconizero@gmail.com>
2022-07-26 11:38:21 -0300

    teste: garante que a rota equipamento retorna status 400 ao adicionar um equipamento sem nome com um token válido

--- src/principal/rotas/rotas-equipamentos.test.ts
+++ src/principal/rotas/rotas-equipamentos.test.ts
@@ -156,5 +156,28 @@ describe('Rotas equipamentos', () => {
         })
         .expect(403)
     })
+
+    test('Deve retornar um status 400 ao adicionar um equipamento sem nome com um token válido', async () => {
+      const senha = await hash('123', 12)
+      const resposta = await Funcionario.create({
+        nome: 'alguém',
+        email: 'email@email.com',
+        senha,
+        administrador: true,
+        areaId: 3
+      })
+      const chave_secreta = process.env.CHAVE_SECRETA //eslint-disable-line
+      const tokenDeAcesso = sign({ id: String(resposta.id) }, (chave_secreta as string), { expiresIn: 60 })
+      const equipamentos = await Equipamento.findAll({ raw: true })
+      await request(app)
+        .put(`/equipamento/${equipamentos[equipamentos.length - 1].id}`)
+        .set('authorization', `Bearer ${tokenDeAcesso}`)
+        .send({
+          tipo: 'tipo alterado',
+          estado: '1',
+          estacaoId: '1'
+        })
+        .expect(400)
+    })
   })
 })
