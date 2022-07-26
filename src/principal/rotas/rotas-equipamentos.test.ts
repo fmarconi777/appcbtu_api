74874552ac68efa38e1ae62ee72139d3f1c9cc2e
Felipe Pedrosa <marconizero@gmail.com>
2022-07-26 11:27:31 -0300

    teste: garante que a rota equipamento retorna status 403 ao alterar um equipamento sem autenticação

--- src/principal/rotas/rotas-equipamentos.test.ts
+++ src/principal/rotas/rotas-equipamentos.test.ts
@@ -5,6 +5,7 @@ import { Funcionario } from '../../infraestrutura/bd/mariadb/models/modelo-funci
 import { hash } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import 'dotenv/config'
+import { Equipamento } from '../../infraestrutura/bd/mariadb/models/modelo-equipamento'
 
 describe('Rotas equipamentos', () => {
   beforeAll(async () => {
@@ -111,8 +112,9 @@ describe('Rotas equipamentos', () => {
         estado: '1',
         estacaoId: '1'
       })
+      const equipamentos = await Equipamento.findAll({ raw: true })
       await request(app)
-        .get('/equipamento/1')
+        .get(`/equipamento/${equipamentos[equipamentos.length - 1].id}`)
         .expect(200)
     })
 
@@ -122,10 +124,24 @@ describe('Rotas equipamentos', () => {
         .expect(404)
     })
 
-    test('Deve retornar status 200 caso um parametro não cadastrado seja fornecido', async () => {
+    test('Deve retornar status 404 caso um parametro não cadastrado seja fornecido', async () => {
       await request(app)
         .get('/equipamento/130100000000000')
-        .expect(200)
+        .expect(404)
+    })
+  })
+
+  describe('PUT', () => {
+    test('Deve retornar um status 403 ao alterar um equipamento sem autenticação', async () => {
+      await request(app)
+        .put('/equipamento/1')
+        .send({
+          nome: 'nome alterado',
+          tipo: 'tipo alterado',
+          estado: '1',
+          estacaoId: '1'
+        })
+        .expect(403)
     })
   })
 })
