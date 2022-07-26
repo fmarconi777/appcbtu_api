a841b669a2e3ef9bf69995796c5344137265ab6d
Felipe Pedrosa <marconizero@gmail.com>
2022-07-26 11:45:34 -0300

    teste: garante que a rota equipamento retorna status 404 caso um parametro inválido seja fornecido

--- src/principal/rotas/rotas-equipamentos.test.ts
+++ src/principal/rotas/rotas-equipamentos.test.ts
@@ -248,5 +248,28 @@ describe('Rotas equipamentos', () => {
         })
         .expect(400)
     })
+
+    test('Deve retornar status 404 caso um parametro inválido seja fornecido', async () => {
+      const senha = await hash('123', 12)
+      const resposta = await Funcionario.create({
+        nome: 'alguém',
+        email: 'email@email.com',
+        senha,
+        administrador: true,
+        areaId: 3
+      })
+      const chave_secreta = process.env.CHAVE_SECRETA //eslint-disable-line
+      const tokenDeAcesso = sign({ id: String(resposta.id) }, (chave_secreta as string), { expiresIn: 60 })
+      await request(app)
+        .put('/equipamento/NaN')
+        .set('authorization', `Bearer ${tokenDeAcesso}`)
+        .send({
+          nome: 'nome alterado',
+          tipo: 'tipo alterado',
+          estado: '1',
+          estacaoId: '1'
+        })
+        .expect(404)
+    })
   })
 })
