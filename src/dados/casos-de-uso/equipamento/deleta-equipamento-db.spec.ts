114494107da0e4a961be79af2e95a7b4469e5e36
Felipe Pedrosa <marconizero@gmail.com>
2022-07-29 11:09:34 -0300

    teste: garante que DeletaEquipamentoDB chama o repositorioConsultaEquipamentoStub com o valor correto

---
+++ src/dados/casos-de-uso/equipamento/deleta-equipamento-db.spec.ts
@@ -0,0 +1,48 @@
+import { ModeloEquipamento } from '../../../dominio/modelos/equipamento'
+import { RepositorioConsultaEquipamento } from '../../protocolos/bd/equipamento/repositorio-consulta-equipamento'
+import { DeletaEquipamentoDB } from './deleta-equipamento-db'
+
+const equipamentoFalso = {
+  id: '1',
+  nome: 'nome_qualquer',
+  tipo: 'tipo_qualquer',
+  estado: 'estado_qualquer',
+  estacaoId: '1'
+}
+
+const makeRepositorioConsultaEquipamentoStub = (): RepositorioConsultaEquipamento => {
+  class RepositorioConsultaEquipamentoStub implements RepositorioConsultaEquipamento {
+    async consultar (id?: number): Promise<ModeloEquipamento | ModeloEquipamento[] | null> {
+      if (id) { // eslint-disable-line
+        return await new Promise(resolve => resolve(equipamentoFalso))
+      }
+      return await new Promise(resolve => resolve([equipamentoFalso]))
+    }
+  }
+  return new RepositorioConsultaEquipamentoStub()
+}
+
+interface SubTipos {
+  sut: DeletaEquipamentoDB
+  repositorioConsultaEquipamentoStub: RepositorioConsultaEquipamento
+}
+
+const id = 1
+
+const makeSut = (): SubTipos => {
+  const repositorioConsultaEquipamentoStub = makeRepositorioConsultaEquipamentoStub()
+  const sut = new DeletaEquipamentoDB(repositorioConsultaEquipamentoStub)
+  return {
+    sut,
+    repositorioConsultaEquipamentoStub
+  }
+}
+
+describe('DeletaEquipamentoDB', () => {
+  test('Deve chamar o repositorioConsultaEquipamentoStub com o valor correto', async () => {
+    const { sut, repositorioConsultaEquipamentoStub } = makeSut()
+    const alterarSpy = jest.spyOn(repositorioConsultaEquipamentoStub, 'consultar')
+    await sut.deletar(id)
+    expect(alterarSpy).toHaveBeenCalledWith(id)
+  })
+})
