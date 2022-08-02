be852ecd8610fdab4fd056462975b8d813b60ed9
Felipe Pedrosa <marconizero@gmail.com>
2022-08-02 10:08:47 -0300

    funcionalidade: garante que CadastroDeAlerta chama o validadorDeEstacao com o valor correto

--- src/dados/casos-de-uso/alerta/cadastro-de-alerta-bd.ts
+++ src/dados/casos-de-uso/alerta/cadastro-de-alerta-bd.ts
@@ -1,15 +1,16 @@
+import { ValidadorBD } from '../../../apresentacao/protocolos/validadorBD'
 import { CadastroAlerta, DadosAlerta } from '../../../dominio/casos-de-uso/alerta/cadastro-de-alerta'
 import { ModeloAlerta } from '../../../dominio/modelos/alerta'
 import { RepositorioAlerta } from '../../protocolos/bd/alerta/repositorio-alerta'
 
 export class CadastroDeAlerta implements CadastroAlerta {
-  private readonly inserirRepositorioAlerta: RepositorioAlerta
+  constructor (
+    private readonly inserirRepositorioAlerta: RepositorioAlerta,
+    private readonly validadorDeEstacao: ValidadorBD
+  ) {}
 
-  constructor (inserirRepositorioAlerta: RepositorioAlerta) {
-    this.inserirRepositorioAlerta = inserirRepositorioAlerta
-  }
-
-  async inserir (dadosAlerta: DadosAlerta): Promise<ModeloAlerta> {
+  async inserir (dadosAlerta: DadosAlerta): Promise<ModeloAlerta | null> {
+    await this.validadorDeEstacao.validar(+dadosAlerta.estacaoId)
     return await this.inserirRepositorioAlerta.inserir(dadosAlerta)
   }
 }
