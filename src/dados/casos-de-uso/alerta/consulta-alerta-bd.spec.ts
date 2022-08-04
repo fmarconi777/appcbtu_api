ffcf33b5e9904ea58b2ed99542949c30e1f25a72
Felipe Pedrosa <marconizero@gmail.com>
2022-08-04 10:55:00 -0300

    teste: garante que ConsultaAlertaBD chama o RepositorioConsultaAlertaPorId com o valor correto caso um id seja fornecido

--- src/dados/casos-de-uso/alerta/consulta-alerta-bd.spec.ts
+++ src/dados/casos-de-uso/alerta/consulta-alerta-bd.spec.ts
@@ -1,6 +1,7 @@
 import { ConsultaAlertaBD } from './consulta-alerta-bd'
 import { RepositorioConsultaAlerta, ModelosAlertas } from '../../protocolos/bd/alerta/repositorio-consulta-alerta-todas'
 import { ModeloAlerta } from '../../../dominio/modelos/alerta'
+import { RepositorioAlertaConsultaPorId } from '../../protocolos/bd/alerta/repositorio-consulta-alerta-por-id'
 
 const makeAlertaFalsa = (): ModeloAlerta => ({
   id: 'id_qualquer',
@@ -14,7 +15,7 @@ const makeAlertaFalsa = (): ModeloAlerta => ({
 
 const makeRepositorioAlerta = (): RepositorioConsultaAlerta => {
   class RepositorioConsultaAlertaStub implements RepositorioConsultaAlerta {
-    async consultaalerta (sigla?: string, id?: number): Promise<ModelosAlertas> {
+    async consultar (sigla?: string, id?: number): Promise<ModelosAlertas> {
       if (sigla) { //eslint-disable-line
         if (id) { //eslint-disable-line
           return await new Promise(resolve => resolve(makeAlertaFalsa()))
@@ -27,62 +28,83 @@ const makeRepositorioAlerta = (): RepositorioConsultaAlerta => {
   return new RepositorioConsultaAlertaStub()
 }
 
+const makeRepositorioAlertaConsultaPorIdStub = (): RepositorioAlertaConsultaPorId => {
+  class RepositorioAlertaConsultaPorIdStub implements RepositorioAlertaConsultaPorId {
+    async consultarPorId (id: number): Promise<ModeloAlerta | null> {
+      return await Promise.resolve(makeAlertaFalsa())
+    }
+  }
+  return new RepositorioAlertaConsultaPorIdStub()
+}
+
 interface SubTipo {
   sut: ConsultaAlertaBD
   repositorioAlertaConsultaStub: RepositorioConsultaAlerta
+  repositorioAlertaConsultaPorIdStub: RepositorioAlertaConsultaPorId
 }
 
 const makeSut = (): SubTipo => {
+  const repositorioAlertaConsultaPorIdStub = makeRepositorioAlertaConsultaPorIdStub()
   const repositorioAlertaConsultaStub = makeRepositorioAlerta()
-  const sut = new ConsultaAlertaBD(repositorioAlertaConsultaStub)
+  const sut = new ConsultaAlertaBD(repositorioAlertaConsultaStub, repositorioAlertaConsultaPorIdStub)
   return {
     sut,
-    repositorioAlertaConsultaStub
+    repositorioAlertaConsultaStub,
+    repositorioAlertaConsultaPorIdStub
   }
 }
 
 describe('ConsultaAlerta', () => {
   test('Deve chamar o RepositorioConsultaAlerta com o valor correto caso somente a sigla seja fornecida', async () => {
     const { sut, repositorioAlertaConsultaStub } = makeSut()
-    const consultarSpy = jest.spyOn(repositorioAlertaConsultaStub, 'consultaalerta')
+    const consultarSpy = jest.spyOn(repositorioAlertaConsultaStub, 'consultar')
     const sigla = 'sigla_qualquer'
-    await sut.consultaalerta(sigla)
+    await sut.consultar(sigla)
     expect(consultarSpy).toHaveBeenCalledWith('sigla_qualquer')
   })
 
   test('Método consultaralerta deve retornar um erro caso o RepositorioConsultaAlerta retorne um erro', async () => {
     const { sut, repositorioAlertaConsultaStub } = makeSut()
-    jest.spyOn(repositorioAlertaConsultaStub, 'consultaalerta').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(repositorioAlertaConsultaStub, 'consultar').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const sigla = 'sigla_qualquer'
-    const respostaConsultar = sut.consultaalerta(sigla)
+    const respostaConsultar = sut.consultar(sigla)
     await expect(respostaConsultar).rejects.toThrow()
   })
 
   test('Deve retornar um array de alertas caso somente a sigla seja fornecida', async () => {
     const { sut } = makeSut()
     const alerta = 'sigla_qualquer'
-    const resposta = await sut.consultaalerta(alerta)
+    const resposta = await sut.consultar(alerta)
     expect(resposta).toEqual([makeAlertaFalsa()])
   })
 
-  test('Método consultaalertaTodas deve retornar um erro caso o RepositorioConsultaAlerta retorne um erro', async () => {
+  test('Deve chamar o RepositorioConsultaAlertaPorId com o valor correto caso um id seja fornecido', async () => {
+    const { sut, repositorioAlertaConsultaPorIdStub } = makeSut()
+    const consultarSpy = jest.spyOn(repositorioAlertaConsultaPorIdStub, 'consultarPorId')
+    const sigla = 'sigla_qualquer'
+    const id = '1'
+    await sut.consultar(sigla, +id)
+    expect(consultarSpy).toHaveBeenCalledWith(1)
+  })
+
+  test('Método consultaAlertaTodas deve retornar um erro caso o RepositorioConsultaAlerta retorne um erro', async () => {
     const { sut, repositorioAlertaConsultaStub } = makeSut()
-    jest.spyOn(repositorioAlertaConsultaStub, 'consultaalerta').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const respostaConsultar = sut.consultaalertaTodas()
+    jest.spyOn(repositorioAlertaConsultaStub, 'consultar').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const respostaConsultar = sut.consultarTodas()
     await expect(respostaConsultar).rejects.toThrow()
   })
 
   test('Deve Retornar um array com todos os alertas caso um parâmetro não seja fornecido', async () => {
     const { sut } = makeSut()
-    const resposta = await sut.consultaalertaTodas()
+    const resposta = await sut.consultarTodas()
     expect(resposta).toEqual([makeAlertaFalsa()])
   })
 
   test('Método consultaralerta deve retornar null caso o RepositorioConsultaAlerta não encontre um alerta para o parametro', async () => {
     const { sut, repositorioAlertaConsultaStub } = makeSut()
-    jest.spyOn(repositorioAlertaConsultaStub, 'consultaalerta').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    jest.spyOn(repositorioAlertaConsultaStub, 'consultar').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const alerta = 'sigla_qualquer'
-    const respostaConsultar = await sut.consultaalerta(alerta)
+    const respostaConsultar = await sut.consultar(alerta)
     expect(respostaConsultar).toBeNull()
   })
 })
