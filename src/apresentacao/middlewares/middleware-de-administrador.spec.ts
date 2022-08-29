02a25bdf760dcbd5c1720cee530880fbd9627d15
Felipe Pedrosa <marconizero@gmail.com>
2022-08-29 16:22:50 -0300

    teste: garante que o MiddlewareDeAdministrador chama o cadastroAdministrador com o valor correto depois que o usuário inserir um valor

--- src/apresentacao/middlewares/middleware-de-administrador.spec.ts
+++ src/apresentacao/middlewares/middleware-de-administrador.spec.ts
@@ -1,5 +1,7 @@
+import { CadastroAdministrador } from '../../dominio/casos-de-uso/middleware/cadastro-de-adminstrador'
 import { ConsultaAdministrador } from '../../dominio/casos-de-uso/middleware/consulta-administrador'
 import { MiddlewareDeAdministrador } from './middleware-de-administrador'
+import ReadLine from 'node:readline'
 
 const makeConsultaAdministradorStub = (): ConsultaAdministrador => {
   class ConsultaAdministradorStub implements ConsultaAdministrador {
@@ -10,17 +12,29 @@ const makeConsultaAdministradorStub = (): ConsultaAdministrador => {
   return new ConsultaAdministradorStub()
 }
 
+const makeCadastroAdministradorStub = (): CadastroAdministrador => {
+  class CadastroAdministradorStub implements CadastroAdministrador {
+    async cadastrar (senha: string): Promise<string> {
+      return await Promise.resolve('Administrador cadastrado com sucesso!')
+    }
+  }
+  return new CadastroAdministradorStub()
+}
+
 interface SubTipos {
   sut: MiddlewareDeAdministrador
   consultaAdministradorStub: ConsultaAdministrador
+  cadastroAdministradorStub: CadastroAdministrador
 }
 
 const makeSut = (): SubTipos => {
+  const cadastroAdministradorStub = makeCadastroAdministradorStub()
   const consultaAdministradorStub = makeConsultaAdministradorStub()
-  const sut = new MiddlewareDeAdministrador(consultaAdministradorStub)
+  const sut = new MiddlewareDeAdministrador(consultaAdministradorStub, cadastroAdministradorStub)
   return {
     sut,
-    consultaAdministradorStub
+    consultaAdministradorStub,
+    cadastroAdministradorStub
   }
 }
 
@@ -31,5 +45,16 @@ describe('Middleware de criação de conta de administrador', () => {
     const resposta = await sut.tratarInput()
     expect(resposta).toBeUndefined()
     expect(consultarSpy).toHaveBeenCalled()
+  })
+
+  test('Deve chamar o cadastroAdministrador com o valor correto depois que o usuário inserir um valor', async () => {
+    const { sut, cadastroAdministradorStub } = makeSut()
+    const cadastrarSpy = jest.spyOn(cadastroAdministradorStub, 'cadastrar')
+    jest.spyOn(ReadLine, 'createInterface').mockReturnValueOnce({
+      question: jest.fn().mockImplementationOnce((texto, input) => input('123')),
+      close: jest.fn().mockImplementationOnce(() => undefined)
+    } as any)
+    await sut.tratarInput()
+    expect(cadastrarSpy).toHaveBeenCalledWith('123')
   })
 })
