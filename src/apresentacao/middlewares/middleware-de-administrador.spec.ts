3bc213983c7f7644734f3824db598b78a2112521
Felipe Pedrosa <marconizero@gmail.com>
2022-08-29 14:09:42 -0300

    teste: garante que o MiddlewareDeAdministrador retorna um erro caso o consultaAdministrador retorne um erro

---
+++ src/apresentacao/middlewares/middleware-de-administrador.spec.ts
@@ -0,0 +1,34 @@
+import { ConsultaAdministrador } from '../../dominio/casos-de-uso/middleware/consulta-administrador'
+import { MiddlewareDeAdministrador } from './middleware-de-administrador'
+
+const makeConsultaAdministradorStub = (): ConsultaAdministrador => {
+  class ConsultaAdministradorStub implements ConsultaAdministrador {
+    async consultar (): Promise<boolean> {
+      return await Promise.resolve(false)
+    }
+  }
+  return new ConsultaAdministradorStub()
+}
+
+interface SubTipos {
+  sut: MiddlewareDeAdministrador
+  consultaAdministradorStub: ConsultaAdministrador
+}
+
+const makeSut = (): SubTipos => {
+  const consultaAdministradorStub = makeConsultaAdministradorStub()
+  const sut = new MiddlewareDeAdministrador(consultaAdministradorStub)
+  return {
+    sut,
+    consultaAdministradorStub
+  }
+}
+
+describe('Middleware de criação de conta de administrador', () => {
+  test('Deve retornar um erro caso o consultaAdministrador retorne um erro', async () => {
+    const { sut, consultaAdministradorStub } = makeSut()
+    jest.spyOn(consultaAdministradorStub, 'consultar').mockReturnValueOnce(Promise.reject(new Error()))
+    const resposta = await sut.tratarInput()
+    expect(resposta).toEqual(console.log(new Error()))
+  })
+})
