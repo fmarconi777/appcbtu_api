bd37c55567d1114f4d94a78bf19b34c2ce7e7ef0
Felipe Pedrosa <marconizero@gmail.com>
2022-08-29 16:22:22 -0300

    funcionalidade: garante que o MiddlewareDeAdministrador chama o cadastroAdministrador com o valor correto depois que o usuário inserir um valor

--- src/apresentacao/middlewares/middleware-de-administrador.ts
+++ src/apresentacao/middlewares/middleware-de-administrador.ts
@@ -1,14 +1,31 @@
 import { Administrador } from '../protocolos/administrador'
+import ReadLine from 'node:readline'
+import { CadastroAdministrador } from '../../dominio/casos-de-uso/middleware/cadastro-de-adminstrador'
 import { ConsultaAdministrador } from '../../dominio/casos-de-uso/middleware/consulta-administrador'
 
 export class MiddlewareDeAdministrador implements Administrador {
   constructor (
-    private readonly consultaAdministrador: ConsultaAdministrador
+    private readonly consultaAdministrador: ConsultaAdministrador,
+    private readonly cadastroAdministrador: CadastroAdministrador
   ) {}
 
   async tratarInput (): Promise<void> {
     try {
-      await this.consultaAdministrador.consultar()
+      if (!await this.consultaAdministrador.consultar()) {
+        const leitor = ReadLine.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        })
+        let senha: string = ''
+
+        leitor.question('Insira uma senha para a conta admin: ', (input) => {
+          senha = input
+          leitor.close()
+        })
+
+        console.log(await this.cadastroAdministrador.cadastrar(senha))
+        console.log('Conta admin cadastrada com sucesso')
+      }
     } catch (erro: any) {
       console.error(erro)
     }
