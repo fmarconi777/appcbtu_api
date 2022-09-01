ba634beac774999667d906eddb6dacb8ac5c392c
Felipe Pedrosa <marconizero@gmail.com>
2022-09-01 09:08:56 -0300

    funcionalidade: garante que o CadastroAdministradorBD chama o repositorioCadastroFuncionario com os valores corretos

--- src/dados/casos-de-uso/middleware/administrador/cadastro-de-administrador.ts
+++ src/dados/casos-de-uso/middleware/administrador/cadastro-de-administrador.ts
@@ -1,13 +1,23 @@
 import { CadastroAdministrador } from '../../../../dominio/casos-de-uso/middleware/administrador/cadastro-de-adminstrador'
+import { RepositorioFuncionario } from '../../../protocolos/bd/funcionario/repositorio-funcionario'
 import { GeradorDeHash } from '../../../protocolos/criptografia/gerador-de-hash'
 
 export class CadastroAdministradorBD implements CadastroAdministrador {
   constructor (
-    private readonly geradorDeHash: GeradorDeHash
+    private readonly geradorDeHash: GeradorDeHash,
+    private readonly repositorioCadastroFuncionario: RepositorioFuncionario
   ) {}
 
   async cadastrar (senha: string, email: string): Promise<string> {
-    await this.geradorDeHash.gerar(senha)
+    const senhaHashed = await this.geradorDeHash.gerar(senha)
+    const contaAdministrador = {
+      nome: 'admin',
+      email: email,
+      senha: senhaHashed,
+      administrador: 'true',
+      areaId: '9'
+    }
+    await this.repositorioCadastroFuncionario.adicionar(contaAdministrador)
     return await Promise.resolve('')
   }
 }
