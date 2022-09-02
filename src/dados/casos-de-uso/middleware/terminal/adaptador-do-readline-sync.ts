b384170098caf99176aaae6f8b476b3ecc2b6307
Felipe Pedrosa <marconizero@gmail.com>
2022-09-02 17:15:00 -0300

    funcionalidade: garante que o AdaptadorDoReadlineSync chama o método perguntarSenha com os valores corretos

--- src/dados/casos-de-uso/middleware/terminal/adaptador-do-readline-sync.ts
+++ src/dados/casos-de-uso/middleware/terminal/adaptador-do-readline-sync.ts
@@ -1,9 +1,15 @@
 import { LeitorDeTerminal } from '../../../../dominio/casos-de-uso/middleware/terminal/leitor-de-terminal'
 import readlineSync from 'readline-sync'
+import { LeitorDeSenhaTerminal } from '../../../../dominio/casos-de-uso/middleware/terminal/leitor-de-senha-terminal'
 
-export class AdaptadorDoReadlineSync implements LeitorDeTerminal {
+export class AdaptadorDoReadlineSync implements LeitorDeTerminal, LeitorDeSenhaTerminal {
   perguntar (pergunta: any): string {
     const input = readlineSync.question(pergunta)
     return input
+  }
+
+  perguntarSenha (pergunta: any): string {
+    readlineSync.questionNewPassword(pergunta)
+    return ''
   }
 }
