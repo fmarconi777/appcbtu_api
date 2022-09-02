b754fd8dc1f5bb5c6077cd4aca626957f7f325b5
Felipe Pedrosa <marconizero@gmail.com>
2022-09-02 15:21:07 -0300

    teste: garante que o AdaptadorDoReadlineSync chama o método perguntar com os valores corretos

---
+++ src/dados/casos-de-uso/middleware/terminal/adaptador-do-readline-sync.spec.ts
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync'
+import { LeitorDeTerminal } from '../../../../dominio/casos-de-uso/middleware/terminal/leitor-de-terminal'
+import { AdaptadorDoReadlineSync } from './adaptador-do-readline-sync'
+
+jest.mock('readline-sync', () => ({
+  question (): string {
+    return 'Input_qualquer'
+  }
+}))
+
+const makeSut = (): LeitorDeTerminal => {
+  return new AdaptadorDoReadlineSync()
+}
+
+describe('AdaptadorDoReadlineSync', () => {
+  describe('Método perguntar', () => {
+    test('Deve chamar o método perguntar com os valores corretos', () => {
+      const sut = makeSut()
+      const questionSpy = jest.spyOn(readlineSync, 'question')
+      sut.perguntar('Instrução qualquer')
+      expect(questionSpy).toHaveBeenCalledWith('Instrução qualquer')
+    })
+  })
+})
