9aa70c297c5850d115efc4e28f3bea722a37e6bf
Felipe Pedrosa <marconizero@gmail.com>
2022-09-05 14:38:47 -0300

    teste: garante que o ControladorDeAlerta retorna código 400 se um id de estação não for fornecido

--- src/apresentacao/controladores/alerta.spec.ts
+++ src/apresentacao/controladores/alerta.spec.ts
@@ -462,5 +462,22 @@ describe('Controlador de Alerta', () => {
       expect(respostaHttp.status).toBe(400)
       expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('ativo'))
     })
+
+    test('Deve retornar código 400 se um id de estação não for fornecido', async () => {
+      const { sut } = makeSut()
+      const requisicaoHttp = {
+        corpo: {
+          descricao: 'qualquer_descricao',
+          prioridade: 'qualquer_prioridade',
+          dataInicio: 'iniciodata_qualquer',
+          dataFim: 'fimdata_qualquer',
+          ativo: 'ativo_qualquer'
+        },
+        metodo: 'PUT'
+      }
+      const respostaHttp = await sut.tratar(requisicaoHttp)
+      expect(respostaHttp.status).toBe(400)
+      expect(respostaHttp.corpo).toEqual(new ErroFaltaParametro('estacaoId'))
+    })
   })
 })
