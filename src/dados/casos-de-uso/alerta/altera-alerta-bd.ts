36a3597e44a135d001fec044db62c9129235184f
Felipe Pedrosa <marconizero@gmail.com>
2022-09-06 14:17:13 -0300

    funcionalidade: garante que o AlteraAlertaBD chama o validadorDeEstacao com o parametro correto

--- src/dados/casos-de-uso/alerta/altera-alerta-bd.ts
+++ src/dados/casos-de-uso/alerta/altera-alerta-bd.ts
@@ -3,11 +3,15 @@ import { AlteraAlerta } from '../../../dominio/casos-de-uso/alerta/altera-alerta
 import { ModeloAlerta } from '../../../dominio/modelos/alerta'
 
 export class AlteraAlertaBD implements AlteraAlerta {
-  constructor (private readonly validadorDeAlerta: ValidadorBD) {}
+  constructor (
+    private readonly validadorDeAlerta: ValidadorBD,
+    private readonly validadorDeEstacao: ValidadorBD
+  ) {}
 
   async alterar (dados: ModeloAlerta): Promise<string | null> {
     const alertaValido = await this.validadorDeAlerta.validar(+dados.id)
     if (alertaValido) {
+      await this.validadorDeEstacao.validar(+dados.estacaoId)
       return await Promise.resolve('')
     }
     return null
