4995a38f3fcb7e364a118e3da7f493fc2c49cd58
Felipe Pedrosa <marconizero@gmail.com>
2022-09-06 15:39:40 -0300

    funcionalidade: garante que o AlteraAlertaBD chama o repositorioAlteraAlerta com o parametro correto

--- src/dados/casos-de-uso/alerta/altera-alerta-bd.ts
+++ src/dados/casos-de-uso/alerta/altera-alerta-bd.ts
@@ -1,11 +1,13 @@
 import { ValidadorBD } from '../../protocolos/utilidades/validadorBD'
 import { AlertaValidado, AlteraAlerta } from '../../../dominio/casos-de-uso/alerta/altera-alerta'
 import { ModeloAlerta } from '../../../dominio/modelos/alerta'
+import { RepositorioAlteraAlerta } from '../../protocolos/bd/alerta/repositorio-altera-alerta'
 
 export class AlteraAlertaBD implements AlteraAlerta {
   constructor (
     private readonly validadorDeAlerta: ValidadorBD,
-    private readonly validadorDeEstacao: ValidadorBD
+    private readonly validadorDeEstacao: ValidadorBD,
+    private readonly repositorioAlteraAlerta: RepositorioAlteraAlerta
   ) {}
 
   async alterar (dados: ModeloAlerta): Promise<AlertaValidado> {
@@ -13,6 +15,7 @@ export class AlteraAlertaBD implements AlteraAlerta {
     if (alertaValido) {
       const estacaoValida = await this.validadorDeEstacao.validar(+dados.estacaoId)
       if (estacaoValida) {
+        await this.repositorioAlteraAlerta.alterar(dados)
         return await Promise.resolve({ valido: true, resposta: '' })
       }
       return { valido: false, resposta: 'estacaoId' }
---
+++ src/dados/protocolos/bd/alerta/repositorio-altera-alerta.ts
@@ -0,0 +1,5 @@
+import { ModeloAlerta } from '../../../../dominio/modelos/alerta'
+
+export interface RepositorioAlteraAlerta {
+  alterar: (dados: ModeloAlerta) => Promise<string>
+}
