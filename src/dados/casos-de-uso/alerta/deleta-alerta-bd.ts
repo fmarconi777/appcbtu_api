2ad57dc478f9f798cf78b53f1b85e3734cfb3849
Felipe Pedrosa <marconizero@gmail.com>
2022-09-14 16:31:06 -0300

    funcionalidade: garante o DeletaAlertaBD chama o repositorioAlteraAlertaAtivo com o valor correto

--- src/dados/casos-de-uso/alerta/deleta-alerta-bd.ts
+++ src/dados/casos-de-uso/alerta/deleta-alerta-bd.ts
@@ -1,14 +1,17 @@
 import { DeletaAlerta } from '../../../dominio/casos-de-uso/alerta/deleta-alerta'
+import { RepositorioAlteraAlertaAtivo } from '../../protocolos/bd/alerta/repositorio-altera-alerta-ativo'
 import { ValidadorBD } from '../../protocolos/utilidades/validadorBD'
 
 export class DeletaAlertaBD implements DeletaAlerta {
   constructor (
-    private readonly validadorDeAlerta: ValidadorBD
+    private readonly validadorDeAlerta: ValidadorBD,
+    private readonly repositorioAlteraAlertaAtivo: RepositorioAlteraAlertaAtivo
   ) {}
 
   async deletar (id: number): Promise<string | null> {
     const alertaValido = await this.validadorDeAlerta.validar(id)
     if (alertaValido) {
+      await this.repositorioAlteraAlertaAtivo.alterarAtivo(false, id)
       return await Promise.resolve('')
     }
     return null
