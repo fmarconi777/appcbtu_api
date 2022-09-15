78a7a396129eba7b94e24ea6b2f68a229d486c52
Felipe Pedrosa <marconizero@gmail.com>
2022-09-15 14:57:40 -0300

    teste: garante o método DELETE na rota alerta retorna status 200 ao deletar um alerta com um token válido

--- src/principal/rotas/rotas-alerta.test.ts
+++ src/principal/rotas/rotas-alerta.test.ts
@@ -369,5 +369,30 @@ describe('Rotas Alerta', () => {
         .set('authorization', `Bearer ${tokenDeAcesso}`)
         .expect(404)
     })
+
+    test('Deve retornar status 200 ao deletar um alerta com um token válido', async () => {
+      const senha = await hash('123', 12)
+      const resposta = await Funcionario.create({
+        nome: 'alguém',
+        email: 'email@email.com',
+        senha,
+        administrador: true,
+        areaId: 3
+      })
+      const chave_secreta = process.env.CHAVE_SECRETA //eslint-disable-line
+      const tokenDeAcesso = sign({ id: String(resposta.id) }, (chave_secreta as string), { expiresIn: 60 })
+      await request(app).post('/alerta').set('authorization', `Bearer ${tokenDeAcesso}`).send({
+        descricao: 'Estação Parada!',
+        prioridade: 'Altissima',
+        dataInicio: '2022-02-05',
+        dataFim: '2025-02-05',
+        ativo: 'true',
+        estacaoId: '1'
+      })
+      await request(app)
+        .delete('/alerta/1')
+        .set('authorization', `Bearer ${tokenDeAcesso}`)
+        .expect(200)
+    })
   })
 })
