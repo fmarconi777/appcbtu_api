4a446a2ac62fcb4f0ec636cc825c10f1f47c5f05
Felipe Pedrosa <marconizero@gmail.com>
2022-09-20 15:35:46 -0300

    refatoracao: adiciona validaArea ao ConsultaAreaBD

--- src/dados/casos-de-uso/area/consulta-area-bd.ts
+++ src/dados/casos-de-uso/area/consulta-area-bd.ts
@@ -1,17 +1,25 @@
 import { ConsultaArea } from '../../../dominio/casos-de-uso/area/consulta-area'
 import { ModeloArea } from '../../../dominio/modelos/area'
 import { RepositorioArea } from '../../protocolos/bd/area/repositorio-area'
+import { ValidadorBD } from '../../protocolos/utilidades/validadorBD'
 
 export class ConsultaAreaBD implements ConsultaArea {
-  constructor (private readonly repositorioArea: RepositorioArea) {}
+  constructor (
+    private readonly validaArea: ValidadorBD,
+    private readonly repositorioArea: RepositorioArea
+  ) {}
 
   async consultarTodas (): Promise<ModeloArea[]> {
     const resposta = await this.repositorioArea.consultar()
     return resposta
   }
 
-  async consultar (area: string): Promise<ModeloArea> {
-    const resposta = await this.repositorioArea.consultar(area)
-    return resposta
+  async consultar (area: string): Promise<ModeloArea | null> {
+    const areValida = await this.validaArea.validar(area)
+    if (areValida) {
+      const resposta = await this.repositorioArea.consultar(area)
+      return resposta
+    }
+    return null
   }
 }
