fe6f07555248df877d2b10c3061c917846973287
Felipe Pedrosa <marconizero@gmail.com>
2022-09-26 09:10:59 -0300

    refatoracao: adiciona validação do equipamentoId ao CadastroDeFalhaBD

--- src/dados/casos-de-uso/falha/cadastro-de-falha-bd.spec.ts
+++ src/dados/casos-de-uso/falha/cadastro-de-falha-bd.spec.ts
@@ -1,6 +1,7 @@
 import { ModeloFalha } from '../../../dominio/modelos/falha'
 import { RepositorioCadastroFalha } from '../../protocolos/bd/falha/repositorio-cadastro-falha'
 import { CadastroDeFalhaBD } from './cadastro-de-falha-bd'
+import { ValidadorBD } from '../../protocolos/utilidades/validadorBD'
 
 const dadosFalsos = (): ModeloFalha => {
   const data = new Date(Date.now() - 10800000).toISOString()
@@ -9,6 +10,15 @@ const dadosFalsos = (): ModeloFalha => {
     dataCriacao: (data.substring(0, 19) + 'Z'),
     equipamentoId: 'equipamentoId_qualquer'
   }
+}
+
+const makeValidaEquipamentoStub = (): ValidadorBD => {
+  class ValidaEquipamentoStub implements ValidadorBD {
+    async validar (parametro: any): Promise<boolean> {
+      return await new Promise(resolve => resolve(true))
+    }
+  }
+  return new ValidaEquipamentoStub()
 }
 
 const makeRepositorioCadastroFalhaStub = (): RepositorioCadastroFalha => {
@@ -22,19 +32,55 @@ const makeRepositorioCadastroFalhaStub = (): RepositorioCadastroFalha => {
 
 interface SubTipos {
   sut: CadastroDeFalhaBD
+  validaEquipamentoStub: ValidadorBD
   repositorioCadastroFalhaStub: RepositorioCadastroFalha
 }
 
 const makeSut = (): SubTipos => {
   const repositorioCadastroFalhaStub = makeRepositorioCadastroFalhaStub()
-  const sut = new CadastroDeFalhaBD(repositorioCadastroFalhaStub)
+  const validaEquipamentoStub = makeValidaEquipamentoStub()
+  const sut = new CadastroDeFalhaBD(validaEquipamentoStub, repositorioCadastroFalhaStub)
   return {
     sut,
+    validaEquipamentoStub,
     repositorioCadastroFalhaStub
   }
 }
 
 describe('CadastroDefalhaBD', () => {
+  test('Deve chamar o validaEquipamento com o valor correto', async () => {
+    const { sut, validaEquipamentoStub } = makeSut()
+    const validarSpy = jest.spyOn(validaEquipamentoStub, 'validar')
+    const dados: any = {
+      numFalha: 'numFalha_qualquer',
+      equipamentoId: 'equipamentoId_qualquer'
+    }
+    await sut.inserir(dados)
+    expect(validarSpy).toHaveBeenCalledWith(dados.equipamentoId)
+  })
+
+  test('Deve retornar um erro caso o validaEquipamento retorne um erro', async () => {
+    const { sut, validaEquipamentoStub } = makeSut()
+    jest.spyOn(validaEquipamentoStub, 'validar').mockReturnValueOnce(Promise.reject(new Error()))
+    const dados: any = {
+      numFalha: 'numFalha_qualquer',
+      equipamentoId: 'equipamentoId_qualquer'
+    }
+    const resposta = sut.inserir(dados)
+    await expect(resposta).rejects.toThrow()
+  })
+
+  test('Deve retornar null caso o validaEquipamento retorne false', async () => {
+    const { sut, validaEquipamentoStub } = makeSut()
+    jest.spyOn(validaEquipamentoStub, 'validar').mockReturnValueOnce(Promise.resolve(false))
+    const dados: any = {
+      numFalha: 'numFalha_qualquer',
+      equipamentoId: 'equipamentoId_qualquer'
+    }
+    const resposta = await sut.inserir(dados)
+    expect(resposta).toBeNull()
+  })
+
   test('Deve chamar o repositorioFalha com os valores corretos', async () => {
     const { sut, repositorioCadastroFalhaStub } = makeSut()
     const inserirSpy = jest.spyOn(repositorioCadastroFalhaStub, 'inserir')
--- src/dados/casos-de-uso/falha/cadastro-de-falha-bd.ts
+++ src/dados/casos-de-uso/falha/cadastro-de-falha-bd.ts
@@ -1,14 +1,22 @@
 import { CadastroDeFalha } from '../../../dominio/casos-de-uso/falha/cadastro-de-falha'
 import { ModeloFalha } from '../../../dominio/modelos/falha'
 import { RepositorioCadastroFalha } from '../../protocolos/bd/falha/repositorio-cadastro-falha'
+import { ValidadorBD } from '../../protocolos/utilidades/validadorBD'
 
 export class CadastroDeFalhaBD implements CadastroDeFalha {
-  constructor (private readonly repositorioCadastroFalha: RepositorioCadastroFalha) {}
+  constructor (
+    private readonly validaEquipamento: ValidadorBD,
+    private readonly repositorioCadastroFalha: RepositorioCadastroFalha
+  ) {}
 
-  async inserir (dados: ModeloFalha): Promise<string> {
-    const data = new Date(Date.now() - 10800000).toISOString()
-    const dataCriacao = (data.substring(0, 19) + 'Z')
-    const falha = Object.assign({}, dados, { dataCriacao })
-    return await this.repositorioCadastroFalha.inserir(falha)
+  async inserir (dados: ModeloFalha): Promise<string | null> {
+    const equipamentoIdValido = await this.validaEquipamento.validar(dados.equipamentoId)
+    if (equipamentoIdValido) {
+      const data = new Date(Date.now() - 10800000).toISOString()
+      const dataCriacao = (data.substring(0, 19) + 'Z')
+      const falha = Object.assign({}, dados, { dataCriacao })
+      return await this.repositorioCadastroFalha.inserir(falha)
+    }
+    return null
   }
 }
