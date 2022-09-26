5e2d80887564d874242ca65735d7dc3d0be8afe4
Felipe Pedrosa <marconizero@gmail.com>
2022-09-26 09:50:26 -0300

    refatoracao: adiciona teste a rota de falha no método POST

--- src/principal/rotas/rotas-falha.test.ts
+++ src/principal/rotas/rotas-falha.test.ts
@@ -46,6 +46,27 @@ describe('Rotas falha', () => {
         .expect(403)
     })
 
+    test('Deve retornar status 404 ao adicionar uma falha com equipamentoId inválido', async () => {
+      const senha = await hash('123', 12)
+      const resposta = await Funcionario.create({
+        nome: 'alguém',
+        email: 'email@email.com',
+        senha,
+        administrador: true,
+        areaId: 3
+      })
+      const chave_secreta = process.env.CHAVE_SECRETA //eslint-disable-line
+      const tokenDeAcesso = sign({ id: String(resposta.id) }, (chave_secreta as string), { expiresIn: 60 })
+      await request(app)
+        .post('/falha')
+        .set('authorization', `Bearer ${tokenDeAcesso}`)
+        .send({
+          numFalha: '1234',
+          equipamentoId: 'NaN'
+        })
+        .expect(404)
+    })
+
     test('Deve retornar um status 200 ao adicionar uma falha com um token válido', async () => {
       const senha = await hash('123', 12)
       const resposta = await Funcionario.create({
