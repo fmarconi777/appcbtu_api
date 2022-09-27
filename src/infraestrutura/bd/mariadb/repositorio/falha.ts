2e6db275942cbddff6d97b6d3dba16fd02b8fc49
Felipe Pedrosa <marconizero@gmail.com>
2022-09-27 15:50:36 -0300

    funcionalidade: garante RepositorioFalhaMariaDB retorna um array com todas as falhas em caso de sucesso se um parâmetro não seja fornecido

--- src/infraestrutura/bd/mariadb/repositorio/falha.ts
+++ src/infraestrutura/bd/mariadb/repositorio/falha.ts
@@ -1,13 +1,21 @@
 import { RepositorioCadastroFalha } from '../../../../dados/protocolos/bd/falha/repositorio-cadastro-falha'
+import { RepositorioConsultaFalha } from '../../../../dados/protocolos/bd/falha/repositorio-consulta-falha'
 import { DadosFalha } from '../../../../dominio/casos-de-uso/falha/cadastro-de-falha'
+import { ModeloFalha } from '../../../../dominio/modelos/falha'
 import { AuxiliaresMariaDB } from '../auxiliares/auxiliar-mariadb'
 import { Falha } from '../models/modelo-falha'
 
-export class RepositorioFalhaMariaDB implements RepositorioCadastroFalha {
+export class RepositorioFalhaMariaDB implements
+RepositorioCadastroFalha,
+RepositorioConsultaFalha {
   async inserir (dados: DadosFalha): Promise<string> {
     AuxiliaresMariaDB.verificaConexao()
     await Falha.create(this.transformaDados(dados))
     return 'Falha cadastrada com sucesso'
+  }
+
+  async consultar (id?: number | undefined): Promise<ModeloFalha | ModeloFalha[] | null> {
+    return await Falha.findAll() as any
   }
 
   private transformaDados (dadosFalha: DadosFalha): any {
