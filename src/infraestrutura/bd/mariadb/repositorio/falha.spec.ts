1b7cfb0eae7c8b99dad69c7037afcf4a26be38f0
Felipe Pedrosa <marconizero@gmail.com>
2022-09-27 15:50:48 -0300

    teste: garante RepositorioFalhaMariaDB retorna um array com todas as falhas em caso de sucesso se um parâmetro não seja fornecido

--- src/infraestrutura/bd/mariadb/repositorio/falha.spec.ts
+++ src/infraestrutura/bd/mariadb/repositorio/falha.spec.ts
@@ -18,15 +18,34 @@ describe('RepositorioFalhaMariaDB', () => {
     await Falha.destroy({ truncate: true, cascade: false })
   })
 
-  test('Deve retornar a mensagem "Falha cadastrada com sucesso" em caso de sucesso ao inserir uma falha', async () => {
-    const sut = new RepositorioFalhaMariaDB()
-    const equipamentos = await Equipamento.findAll({ raw: true })
-    const dadosFalsos = {
-      numFalha: '12345',
-      dataCriacao: '2011-04-11T10:20:30Z',
-      equipamentoId: (equipamentos[equipamentos.length - 1].id).toString()
-    }
-    const resposta = await sut.inserir(dadosFalsos)
-    expect(resposta).toEqual('Falha cadastrada com sucesso')
+  describe('Método inserir', () => {
+    test('Deve retornar a mensagem "Falha cadastrada com sucesso" em caso de sucesso ao inserir uma falha', async () => {
+      const sut = new RepositorioFalhaMariaDB()
+      const equipamentos = await Equipamento.findAll({ raw: true })
+      const dadosFalsos = {
+        numFalha: '12345',
+        dataCriacao: '2011-04-11T10:20:30Z',
+        equipamentoId: (equipamentos[equipamentos.length - 1].id).toString()
+      }
+      const resposta = await sut.inserir(dadosFalsos)
+      expect(resposta).toEqual('Falha cadastrada com sucesso')
+    })
+  })
+
+  describe('Método consultar', () => {
+    test('Deve retornar um array com todas as falhas em caso de sucesso se um parâmetro não seja fornecido', async () => {
+      const sut = new RepositorioFalhaMariaDB()
+      const equipamentos = await Equipamento.findAll({ raw: true })
+      const dadosFalsos = {
+        numFalha: '12345',
+        dataCriacao: '2011-04-11T10:20:30Z',
+        equipamentoId: (equipamentos[equipamentos.length - 1].id).toString()
+      }
+      await sut.inserir(dadosFalsos)
+      const resposta: any = await sut.consultar()
+      expect(resposta).toBeTruthy()
+      expect(Array.isArray(resposta)).toBeTruthy()
+      expect(resposta.length).toBeGreaterThan(0)
+    })
   })
 })
