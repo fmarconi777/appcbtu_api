2eaf647bd7ef47fbdf6bde065d36ab0da56fef66
Felipe <marconizero@gmail.com>
2022-10-02 23:00:21 -0300

    funcionalidade: garante AlteraFalhaBD chama o validadorDeEquipamento com o parâmetro correto

--- src/dados/casos-de-uso/falha/altera-falha-bd.ts
+++ src/dados/casos-de-uso/falha/altera-falha-bd.ts
@@ -1,14 +1,17 @@
 import { AlteraFalha, FalhaAlterada, FalhaValida } from '../../../dominio/casos-de-uso/falha/altera-falha'
 import { RepositorioConsultaFalha } from '../../protocolos/bd/falha/repositorio-consulta-falha'
+import { ValidadorBD } from '../../protocolos/utilidades/validadorBD'
 
 export class AlteraFalhaBD implements AlteraFalha {
   constructor (
-    private readonly repositorioConsultaFalha: RepositorioConsultaFalha
+    private readonly repositorioConsultaFalha: RepositorioConsultaFalha,
+    private readonly validaEquipamento: ValidadorBD
   ) {}
 
   async alterar (dados: FalhaAlterada): Promise<FalhaValida> {
     const idValido = await this.repositorioConsultaFalha.consultar(dados.id)
     if (idValido) { //eslint-disable-line
+      await this.validaEquipamento.validar(dados.equipamentoId)
       return {
         falhaInvalida: false,
         parametro: ''
