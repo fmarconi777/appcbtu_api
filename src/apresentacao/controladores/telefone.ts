884b21639b12af1cb778bd788d0317d7457727d1
Felipe Pedrosa <marconizero@gmail.com>
2022-10-11 10:39:34 -0300

    funcionalidade: garante o ControladorDeTelefone retorna a status 400 caso o parametro numero seja inválido

--- src/apresentacao/controladores/telefone.ts
+++ src/apresentacao/controladores/telefone.ts
@@ -1,6 +1,7 @@
 import { requisicaoImpropria, resposta } from '../auxiliares/auxiliar-http'
 import { ErroFaltaParametro } from '../erros/erro-falta-parametro'
 import { ErroMetodoInvalido } from '../erros/erro-metodo-invalido'
+import { ErroParametroInvalido } from '../erros/erro-parametro-invalido'
 import { Controlador } from '../protocolos/controlador'
 import { RequisicaoHttp, RespostaHttp } from '../protocolos/http'
 
@@ -15,6 +16,10 @@ export class ControladorDeTelefone implements Controlador {
           if (!requisicaoHttp.corpo[campo]) { // eslint-disable-line
             return requisicaoImpropria(new ErroFaltaParametro(campo))
           }
+        }
+        const numero = +requisicaoHttp.corpo.numero
+        if (!Number.isInteger(numero) || numero !== Math.abs(numero)) {
+          return requisicaoImpropria(new ErroParametroInvalido('numero'))
         }
         return resposta('')
       }
