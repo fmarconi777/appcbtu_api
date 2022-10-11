3a0ca1b79a4635cc770133d132144fd1b17b0956
Felipe Pedrosa <marconizero@gmail.com>
2022-10-11 14:22:00 -0300

    teste: garante o CadastroDeTelefoneBD chama o validaEstacao com o valor correto

---
+++ src/dados/casos-de-uso/telefone/cadastro-de-telefone-bd.spec.ts
@@ -0,0 +1,37 @@
+import { ValidadorBD } from '../../protocolos/utilidades/validadorBD'
+import { CadastroDeTelefoneBD } from './cadastro-de-telefone-bd'
+
+const numero = 3132505555
+const estacaoId = 1
+
+const makeValidaEstacaoStub = (): ValidadorBD => {
+  class ValidaEstacaoStub implements ValidadorBD {
+    async validar (parametro: string): Promise<boolean> {
+      return await new Promise(resolve => resolve(true))
+    }
+  }
+  return new ValidaEstacaoStub()
+}
+
+interface SubTipos {
+  sut: CadastroDeTelefoneBD
+  validaEstacaoStub: ValidadorBD
+}
+
+const makeSut = (): SubTipos => {
+  const validaEstacaoStub = makeValidaEstacaoStub()
+  const sut = new CadastroDeTelefoneBD(validaEstacaoStub)
+  return {
+    sut,
+    validaEstacaoStub
+  }
+}
+
+describe('CadastroDeTelefoneBD', () => {
+  test('Deve chamar o validaEstacao com o valor correto', async () => {
+    const { sut, validaEstacaoStub } = makeSut()
+    const validarSpy = jest.spyOn(validaEstacaoStub, 'validar')
+    await sut.inserir(numero, estacaoId)
+    expect(validarSpy).toHaveBeenCalledWith(estacaoId)
+  })
+})
