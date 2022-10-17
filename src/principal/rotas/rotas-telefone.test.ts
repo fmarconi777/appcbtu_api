b7928f1288a5ddaa2e4f2b366661d7e7c2167637
Felipe Pedrosa <marconizero@gmail.com>
2022-10-17 10:49:10 -0300

    teste: garante o método POST na rota telefone retorna status 403 ao adicionar um telefone sem autenticação

---
+++ src/principal/rotas/rotas-telefone.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import app from '../config/app'
+import { AuxiliaresMariaDB } from '../../infraestrutura/bd/mariadb/auxiliares/auxiliar-mariadb'
+import { Telefone } from '../../infraestrutura/bd/mariadb/models/modelo-telefone'
+
+describe('Rotas Telefone', () => {
+  beforeAll(async () => {
+    await AuxiliaresMariaDB.conectar()
+    console.log('conexao aberta')
+  })
+
+  afterAll(async () => {
+    await AuxiliaresMariaDB.desconectar()
+    console.log('conexao fechada')
+  })
+
+  beforeEach(async () => {
+    await Telefone.destroy({ truncate: true, cascade: false })
+  })
+
+  describe('POST', () => {
+    test('Deve retornar status 403 ao adicionar um telefone sem autenticação', async () => {
+      await request(app)
+        .post('/telefone')
+        .send({
+          numero: '3132505555',
+          estacaoId: '1'
+        })
+        .expect(403)
+    })
+  })
+})
