c4379e2a400b63436ad8e01c60f3f6d54dd463ce
Felipe Pedrosa <marconizero@gmail.com>
2022-10-17 11:57:21 -0300

    refatoracao: retirado comentário do criarAdmin()

--- src/principal/config/app.ts
+++ src/principal/config/app.ts
@@ -6,5 +6,5 @@ import { middlewareDeAdministrador } from './conta-administrador'
 const app = express()
 configuraMiddlewares(app)
 configuraRotas(app)
-// middlewareDeAdministrador.criarAdmin() // middlewareDeAdministrador deve ser colocado como comentário antes de realizar os testes
+middlewareDeAdministrador.criarAdmin() // middlewareDeAdministrador deve ser colocado como comentário antes de realizar os testes
 export default app
